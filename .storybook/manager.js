8e23b149a6c5df58812666f8655f2eba9aa028c1
Rafael Dias <rdias.ga@gmail.com>
2020-09-17 14:42:05 -0300

    Cria o storybook manager

---
+++ .storybook/manager.js
@@ -0,0 +1,16 @@
+// .storybook/manager.js
+
+import { addons } from '@storybook/addons';
+
+addons.setConfig({
+  isFullscreen: false,
+  showNav: true,
+  showPanel: true,
+  panelPosition: 'right',
+  sidebarAnimations: true,
+  enableShortcuts: true,
+  isToolshown: true,
+  selectedPanel: undefined,
+  initialActive: 'sidebar',
+  showRoots: true,
+});
