30d28e4a924c12ed5202c08dd3d816a04545a558
Rafael Dias <rdias.ga@gmail.com>
2020-09-17 15:39:29 -0300

    Cria tema do storybook

---
+++ .storybook/theme.js
@@ -0,0 +1,33 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+    base: 'light',
+    colorPrimary: '#074fa6',
+    colorSecondary: '#2EB88D',
+  
+    // UI
+    appBg: '#FFFFFF',
+    appContentBg: '#FFFFFF',
+    appBorderColor: 'silver',
+    appBorderRadius: 4,
+  
+    // Tipografia
+    fontBase: '"Muli", sans-serif',
+    fontCode: 'monospace',
+  
+    // Cores de texto
+    textColor: '#212529',
+    textInverseColor: '#ffffffe6',
+  
+    // Cor da Toolbar
+    barSelectedColor: '#074fa6',
+  
+    // Cor dos forms
+    inputBg: '#FFFFFF',
+    inputTextColor: '#142032',
+    inputBorderRadius: 4,
+  
+    brandTitle: 'Cuida',
+    brandUrl: 'https://sysvale.com',
+    // brandImage: cuidaLogo,
+  });
