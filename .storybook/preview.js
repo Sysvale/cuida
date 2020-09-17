673380dcc2ce73d9a1610a0e60d9c8e174bb8ecb
Rafael Dias <rdias.ga@gmail.com>
2020-09-17 15:40:17 -0300

    Habilita a aba de documentação para ser a primeira a única a ser apresentada no storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,23 @@
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+import { addParameters } from "@storybook/vue";
+import CuidaTheme from './theme';
+
+
+addParameters({
+	options: {
+    isFullscreen: false,
+    showPanel: true,
+    panelPosition: 'right',
+		showAddonPanel: true,
+		addonPanelInRight: false,
+		sidebarAnimations: true,
+    isToolshown: true,
+    enableShortcuts: false,
+		theme: CuidaTheme,
+		showRoots: true,
+    showNav: true,
+    initialActive: 'sidebar',
+    selectedPanel: 'controls',
+    showCanvas: false,
+	},
+});
