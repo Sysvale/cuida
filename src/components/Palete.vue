fe50cfd00fb8bfb89cbb49056ac0977c0ee6d5e3
Rafael Dias <rdias.ga@gmail.com>
2020-09-24 11:28:00 -0300

    Passa a mostrar as cores e suas shades no componente Palete

--- src/components/Palete.vue
+++ src/components/Palete.vue
@@ -1,14 +1,90 @@
 <template>
-    <div>
+    <div
+      class="grid"
+    >
+        <div
+          v-for="color in paleteColors"
+          :key="color"
+          class="palete"
+        >
+          <div
+            v-for="(shade, index) in shades"
+            :key="shade"
+            :class='{
+              "first-shade": index === 0,
+              "last-shade": index === shades.length - 1,
+              [`${colorShadeClass(color, shade)}`]: index >= 0,
+            }'
+          />
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      paleteColors: [
+        'turquesa-perry',
+        'verde-piccolo',
+        'azul-sonic',
+        'azul-bidu',
+        'roxo-thanos',
+        'pantera-cor-de-rosa',
+        'vermelho-mario',
+        'laranja-naruto',
+        'amarelo-pikachu',
+        'escala-de-cinza',
+      ],
+      shades: [
+        'base',
+        'light-2',
+        'light-1',
+        'dark-1',
+        'dark-2',
+      ],
+    };
+  },
 
+  methods: {
+    colorShadeClass(color, shade) {
+      return `${color}-${shade}`;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/sass/app.scss';
 
+@each $colorName, $color in $palete {
+  @each $shadeName, $shade in $color {
+    .#{$colorName}-#{$shadeName} {
+      background-color: $shade;
+      width: 250px;
+      height: 50px;
+    }
+  }
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  row-gap: 50px;
+}
+
+.first-shade {
+  border-radius: 8px 8px 0px 0px;
+  height: 100px;
+}
+
+.last-shade {
+  border-radius: 0px 0px 8px 8px;
+}
+
+.palete {
+  width: max-content;
+  box-shadow: 2px 4px 15px #2b343b33;
+  border-radius: 8px;
+}
 </style>
