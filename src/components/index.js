42e615e3f951fbfad79d1a688641749ce76da387
Rafael Dias <rdias.ga@gmail.com>
2020-09-30 00:06:22 -0300

    Exporta componente

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@
 export { default as ProgressBar } from './ProgressBar.vue';
 export { default as Palete } from './Palete.vue';
 export { default as ActionsList } from './ActionsList.vue';
+export { default as RadioButtonGroup } from './RadioButtonGroup.vue';
