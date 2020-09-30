876c4b0a3417842f367c2e03c5350ccdbbdace02
Rafael Dias <rdias.ga@gmail.com>
2020-09-30 13:36:21 -0300

    adiciona prefixo "cds-" nos componentes do cuida

--- src/entry.js
+++ src/entry.js
@@ -3,9 +3,14 @@ import * as components from './components/index';
 // install function executed by Vue.use()
 function install(Vue) {
 	if (install.installed) return;
+
 	install.installed = true;
+
 	Object.keys(components).forEach((componentName) => {
-		Vue.component(componentName, `cds-${components[componentName]}`);
+		Vue.component(
+			`cds-${componentName.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase()}`,
+			components[componentName],
+		);
 	});
 }
 
@@ -17,18 +22,19 @@ const plugin = {
 // To auto-install when vue is found
 /* global window global */
 let GlobalVue = null;
+
 if (typeof window !== 'undefined') {
 	GlobalVue = window.Vue;
 } else if (typeof global !== 'undefined') {
 	GlobalVue = global.Vue;
 }
+
 if (GlobalVue) {
 	GlobalVue.use(plugin);
 }
 
 // Default export is library as a whole, registered via Vue.use()
 export default plugin;
-
 // To allow individual component use, export components
 // each can be registered via Vue.component()
 export * from './components/index';
