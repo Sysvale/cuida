066d27101ae0e5bc73993bf1c2d7174705973f02
Lucas Nascimento <lucas.barbosa.nas@gmail.com>
2020-11-19 15:34:43 -0300

    Ajusta navbar para utilizar o padrão BEM

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,44 +1,44 @@
 <template>
-	<b-nav
-		class="px-2"
-		:class="isLightThemed ? 'bg-cs' : 'bg-liga'"
-	>
-		<component
-			v-for="(item, i) in computedItems"
-			:is="dropdownOrSingleItem(item)"
-			:class="getClass(item)"
-			:id="getElementKey(item, i)"
-			:key="getElementKey(item, i)"
-			:text="item.label"
-			:active="isActive(item)"
-			:to="{name: item.path}"
-			class="mr-4 mt-2"
-			menu-class="py-1"
-			@click="handleClick(item)"
+	<span id="nav-bar">
+		<b-nav
+			class="px-2"
+			:class="isLightThemed ? 'nav-bar__background--light' : 'nav-bar__background--dark'"
 		>
-			<template
-				v-if="isDropdown(item)"
+			<component
+				v-for="(item, i) in computedItems"
+				:is="dropdownOrSingleItem(item)"
+				:class="getClass(item)"
+				:id="getElementKey(item, i)"
+				:key="getElementKey(item, i)"
+				:text="item.label"
+				:active="isActive(item)"
+				:to="{name: item.path}"
+				class="mr-4 mt-2"
+				menu-class="py-1"
+				@click="handleClick(item)"
 			>
-				<b-dropdown-item
-					v-for="(subitem, j) in item.items"
-					:id="getElementKey(subitem, j, true)"
-					:key="getElementKey(subitem, j, true)"
-					:active="isActive(subitem)"
-					:to="{name: item.path}"
-					link-class="py-2"
-					class="py-1"
-					@click.stop="handleClick(subitem, item)"
+				<template
+					v-if="isDropdown(item)"
 				>
-					{{ subitem.label }}
-				</b-dropdown-item>
-			</template>
-			<template
-				v-else
-			>
-				{{ item.label }}
-			</template>
-		</component>
-	</b-nav>
+					<b-dropdown-item
+						v-for="(subitem, j) in item.items"
+						:id="getElementKey(subitem, j, true)"
+						:key="getElementKey(subitem, j, true)"
+						:active="isActive(subitem)"
+						:to="{name: item.path}"
+						@click.stop="handleClick(subitem, item)"
+					>
+						{{ subitem.label }}
+					</b-dropdown-item>
+				</template>
+				<template
+					v-else
+				>
+					{{ item.label }}
+				</template>
+			</component>
+		</b-nav>
+	</span>
 </template>
 
 <script>
@@ -74,7 +74,7 @@ export default {
 		return {
 			internalActiveItem: this.activeItem,
 			internalActiveParent: this.activeItem,
-		}
+		};
 	},
 
 	computed: {
@@ -109,7 +109,7 @@ export default {
 					);
 					if (subitems.length) {
 						[this.internalActiveItem] = subitems.filter(
-							(item) => item.path === this.activeItem.path
+							(item) => item.path === this.activeItem.path,
 						);
 
 						this.internalActiveParent = this.internalActiveItem.parent;
@@ -139,7 +139,7 @@ export default {
 		},
 
 		getElementKey(item, index, subitem = false) {
-			return `${item.label.replace(/\s/g, '')}${index}${ subitem ? '-subitem' : ''}`;
+			return `${item.label.replace(/\s/g, '')}${index}${subitem ? '-subitem' : ''}`;
 		},
 
 		isSubitem(item) {
@@ -156,7 +156,7 @@ export default {
 		},
 
 		getClass(item) {
-			let accClass = this.isLightThemed ? 'cs-mode' : 'liga-mode';
+			let accClass = this.isLightThemed ? 'nav-bar__item--light' : 'nav-bar__item--dark';
 
 			if (
 				this.isDropdown(item)
@@ -170,113 +170,170 @@ export default {
 	},
 };
 </script>
-<style>
-	a {
-		outline: none;
+<style lang="scss">
+@import '../assets/sass/app.scss';
+
+a {
+	outline: none;
+}
+
+#nav-bar .nav-bar__background {
+	padding: 8px 0;
+
+	&--dark {
+		background: $cinza-7;
+
+		.dropdown-menu {
+			background: $cinza-7;
+		}
 	}
 
-	.cs-mode .nav-link:hover, .cs-mode .dropdown-item:hover {
-		color: rgb(255, 255, 255);
+	&--light {
+		background: linear-gradient(0.93deg, $branco 16.45%, $cinza-2 105.67%);
+
+		.dropdown-menu {
+				background: $branco;
+				box-shadow: 2px 2px 4px $cinza-5;
+				border-radius: 2px;
+		}
+	}
+}
+
+#nav-bar .nav-bar__item {
+	margin-top: 8px;
+	margin-right: 24px;
+
+	&--dark {
+		@extend .nav-bar__item;
+
+		.active {
+			color: $branco;
+			border-bottom: 4px solid $verde-piccolo-base;
+			background: $cinza-6;
+			border-radius: 4px  4px 0px 0px;
+		}
+
+		&.active-parent{
+			color: $branco;
+			border-bottom: 4px solid $verde-piccolo-base;
+			background: $cinza-6;
+			border-radius: 4px  4px 0px 0px;
+
+			.show {
+				border-bottom: 4px solid transparent;
+			}
+
+			.dropdown-toggle {
+				color: $branco;
+			}
+		}
+
+		.nav-link {
+			color: $cinza-4;
+
+			&:hover {
+				color: $branco;
+			}
+
+			&.active {
+				color: $branco;
+			}
+		}
+
+		.dropdown-item {
+			color: $cinza-4;
+
+			&:hover {
+				color: $branco;
+				background: transparent;
+			}
+
+			&.active {
+				color: $branco;
+				border-left: 4px solid $verde-piccolo-base;
+				border-bottom: 0px;
+				background: $cinza-6;
+				border-radius: 0px;
+			}
+		}
 	}
 
-	.cs-mode .dropdown-item:hover {
-		background: transparent;
-	}
+	&--light {
+		@extend .nav-bar__item;
 
-	.cs-mode .active, .cs-mode.active-parent {
-		border-bottom: 4px solid rgb(58, 223, 124);
-		background: rgb(90, 90, 90);
-		border-radius: 4px  4px 0px 0px;
-	}
+		.active {
+			color: $cinza-8 !important;
+			border-bottom: 4px solid $azul-bidu-dark-1;
+			background: $cinza-2;
+			border-radius: 4px  4px 0px 0px;
+		}
 
-	.cs-mode.active-parent.show {
-		border-bottom: 4px solid transparent;
-	}
+		&.active-parent{
+			color: $cinza-8 !important;
+			border-bottom: 4px solid $azul-bidu-dark-1;
+			background: $cinza-2;
+			border-radius: 4px  4px 0px 0px;
 
-	.cs-mode .dropdown-item.active {
-		border-left: 4px solid rgb(58, 223, 124);
-		border-bottom: 0px;
-		background: rgb(90, 90, 90);
-		border-radius: 0px;
-	}
+			.show {
+				border-bottom: 4px solid transparent;
+			}
 
-	.bg-liga {
-		background: linear-gradient(0.93deg, #FFFFFF 16.45%, #F1F5FB 105.67%);
-	}
+			.dropdown-toggle {
+				color: $cinza-8;
+			}
+		}
 
-	.bg-liga .dropdown-menu {
-		background: #fff;
-	}
+		.nav-link {
+			color: $cinza-6;
 
-	.bg-cs, .bg-cs .dropdown-menu {
-		background: rgb(68, 68, 68);
-	}
+			&:hover {
+				color: $cinza-8;
+				background: transparent;
+			}
 
-	.cs-mode .nav-link.active,
-	.cs-mode .dropdown-item.active,
-	.cs-mode.active-parent
-	.dropdown-toggle {
-		color: rgb(255, 255, 255);
-	}
+			.active {
+				color: $cinza-8;
+			}
+		}
 
-	.cs-mode .nav-link, .cs-mode .dropdown-item {
-		color: rgb(208, 210, 213);
-	}
+		.dropdown-item {
+			color: $cinza-6;
 
-	.liga-mode .active, .liga-mode.active-parent {
-		border-bottom: 4px solid rgb(40, 90, 185);
-		background: rgba(40, 90, 185, 0.05);
-		border-radius: 4px  4px 0px 0px;
-	}
+			&:hover {
+				color: $cinza-8;
+				background: transparent;
+			}
 
-	.liga-mode.active-parent.show {
-		border-bottom: 4px solid transparent;
+			&.active {
+				border-left: 4px solid $azul-bidu-dark-1;
+				border-bottom: 0px;
+				background: $cinza-2 !important;
+				border-radius: 0px;
+			}
+		}
 	}
+}
 
-	.liga-mode .nav-link.active,
-	.liga-mode .dropdown-item.active,
-	.liga-mode.active-parent
-	.dropdown-toggle {
-		color: rgb(20, 32, 50);
-	}
+.dropdown-item:active {
+	background: inherit;
+}
 
-	.liga-mode .nav-link, .liga-mode .dropdown-item {
-		color: rgb(106, 117, 128);
-	}
+.dropdown-item {
+	padding-top: 10px;
+	padding-bottom: 10px;
+}
 
-	.liga-mode .nav-link:hover, .liga-mode .dropdown-item:hover {
-		color: rgb(20, 32, 50);
-		background: transparent;
-	}
+.dropdown-menu {
+	margin-top: 4px;
+	border: 0;
+	border-radius: 0;
+}
 
-	.bg-liga .dropdown-menu {
-		background: #fff;
-		box-shadow: 2px 2px 4px rgba(58, 67, 81, 0.16);
-		border-radius: 2px;
-	}
+.dropdown-toggle[aria-expanded="true"]:after {
+	transform: rotate(180deg);
+}
 
-	.liga-mode .dropdown-item.active {
-		border-left: 4px solid rgb(40, 90, 185);
-		border-bottom: 0px;
-		background: rgba(40, 90, 185, 0.05);
-		border-radius: 0px;
-	}
-
-	.dropdown-item:active {
-		background: inherit;
-	}
-
-	.dropdown-menu {
-		margin-top: 4px;
-		border: 0;
-		border-radius: 0;
-	}
-
-	.dropdown-toggle[aria-expanded="true"]:after {
-		transform: rotate(180deg);
-	}
-
-	.dropdown-toggle:after {
-		transition: all 0.4s ease;
-	}
+.dropdown-toggle:after {
+	transition: all 0.4s ease;
+}
 </style>
