8a29e4faa7484c047805ef683bf9301c65e1de5c
Rafael Dias <rafaeldias@MacBook-Pro-de-Rafael.local>
2020-12-04 17:33:03 -0300

    Configura no preview.js no vue-long-click

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -29,11 +29,16 @@ import {
 	Stepper,
 	ProgressCircular,
 	AlertCard,
+	StepperInput,
 } from '../src/components';
 
 import Palete from '../src/Palete.vue';
 import TypographyList from '../src/TypographyList.vue';
 import Space from '../src/Space.vue';
+import { longClickDirective } from 'vue-long-click'
+
+const longClickInstance = longClickDirective({delay: 400, interval: 50});
+Vue.directive('longclick', longClickInstance);
 
 Vue.use(BootstrapVue);
 
@@ -57,6 +62,7 @@ Vue.component('upload-input', UploadInput);
 Vue.component('stepper', Stepper);
 Vue.component('progress-circular', ProgressCircular);
 Vue.component('alert-card', AlertCard);
+Vue.component('stepper-input', StepperInput);
 
 addParameters({
 	options: {
