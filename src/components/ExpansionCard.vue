8c84f18e54c726dea2aa2d77f803c588da42b7d8
Patricia Coelho <patriciacoelho.slv@gmail.com>
2020-12-10 16:30:34 -0300

    Implementa template padrão do card; add prop de icon, mainValue e legend;

--- src/components/ExpansionCard.vue
+++ src/components/ExpansionCard.vue
@@ -20,17 +20,32 @@
 		>
 			<b-icon-arrows-angle-contract />
 		</div>
-		<!--  -->
+		<slot>
+			<div class="expansion-card__body">
+				<div class="expansion-card__content">
+					<h1 class="expansion-card__main-value">
+						{{ mainValue }}
+					</h1>
+					<p class="expansion-card__legend">
+						{{ legend }}
+					</p>
+				</div>
+
+				<div
+					v-if="!!icon"
+					class="expansion-card__icon"
+					:class="`expansion-card__icon--${variant}`"
+				>
+					<b-icon
+						:icon="icon"
+					/>
+				</div>
+			</div>
+		</slot>
 	</div>
 </template>
 
 <script>
-import {
-	BIcon,
-	BIconArrowsAngleExpand,
-	BIconArrowsAngleContract
-} from 'bootstrap-vue';
-
 export default {
 	props: {
 		/**
@@ -41,12 +56,31 @@ export default {
 			type: String,
 			default: 'gray',
 		},
-	},
-
-	components: {
-		BIcon,
-		BIconArrowsAngleContract,
-		BIconArrowsAngleExpand,
+		/**
+		 * Ícone para o template padrão. O valor desse parâmetro deve ser um alias
+		 * de um ícone da biblioteca de ícones do bootstrap. Ex.: 'exclamation-circle'.
+		 * Veja os ícones disponíveis em https://icons.getbootstrap.com/.
+		 */
+		icon: {
+			type: String,
+			default: null,
+		},
+		/**
+		 * Legenda da informação principal do card (no template padrão). Torna-se
+		 * o título do card quando este é expandido.
+		 */
+		legend: {
+			type: String,
+			default: null,
+		},
+		/**
+		 * Informação principal do card (no template padrão), pode ser a quantidade 
+		 * de resultados, por exemplo.
+		 */
+		mainValue: {
+			type: Number,
+			default: null,
+		}
 	},
 
 	data() {
@@ -71,27 +105,10 @@ export default {
 		},
 
 		variantStyle() {
-			switch (this.variant) {
-				case 'turquoise':
-					return 'expansion-card--turquoise';
-				case 'green':
-					return 'expansion-card--green';
-				case 'blue':
-					return 'expansion-card--blue';
-				case 'purple':
-					return 'expansion-card--purple';
-				case 'pink':
-					return 'expansion-card--pink';
-				case 'red':
-					return 'expansion-card--red';
-				case 'orange':
-					return 'expansion-card--orange';
-				case 'yellow':
-					return 'expansion-card--yellow';
-				case 'gray':
-				default:
-					return 'expansion-card--gray';
+			if (!this.variant) {
+				return 'expansion-card--gray';
 			}
+			return `expansion-card--${this.variant}`;
 		},
 	},
 
@@ -108,6 +125,11 @@ export default {
 			case 'expand':
 			case 'contract':
 				this.isExpanded = !this.isExpanded;
+				/**
+				* Evento emitido quando o card é expandido ('true') ou contraído ('false').
+				* @event expanded
+				* @type {Event}
+				*/
 				this.$emit('expanded', this.isExpanded);
 				break;
 			}
@@ -121,6 +143,8 @@ export default {
 
 .expansion-card {
 	display: flex;
+	position: relative;
+	flex-direction: column;
 	align-items: center;
 	@include padding(onidirecional, 3);
 	border-radius: 16px;
@@ -136,56 +160,58 @@ export default {
 		cursor: pointer;
 
 		&.expansion-card {
+			transition: box-shadow .2s ease-in-out;
+
 			&--blue {
-				transition: box-shadow .2s ease-in-out;
+				@extend .expansion-card--hover.expansion-card;
 				border: 1px solid $azul-sonic-light-2;
 				box-shadow: 0 0 2px $azul-sonic-base;
 			}
 
 			&--turquoise {
-				transition: box-shadow .2s ease-in-out;
+				@extend .expansion-card--hover.expansion-card;
 				border: 1px solid $turquesa-perry-light-2;
 				box-shadow: 0 0 2px $turquesa-perry-base;
 			}
 
 			&--green {
-				transition: box-shadow .2s ease-in-out;
+				@extend .expansion-card--hover.expansion-card;
 				border: 1px solid $verde-piccolo-light-2;
 				box-shadow: 0 0 2px $verde-piccolo-base;
 			}
 
 			&--purple {
-				transition: box-shadow .2s ease-in-out;
+				@extend .expansion-card--hover.expansion-card;
 				border: 1px solid $roxo-thanos-light-2;
 				box-shadow: 0 0 2px $roxo-thanos-base;
 			}
 
 			&--pink {
-				transition: box-shadow .2s ease-in-out;
+				@extend .expansion-card--hover.expansion-card;
 				border: 1px solid $pantera-cor-de-rosa-light-2;
 				box-shadow: 0 0 2px $pantera-cor-de-rosa-base;
 			}
 
 			&--red {
-				transition: box-shadow .2s ease-in-out;
+				@extend .expansion-card--hover.expansion-card;
 				border: 1px solid $vermelho-mario-light-2;
 				box-shadow: 0 0 2px $vermelho-mario-base;
 			}
 
 			&--orange {
-				transition: box-shadow .2s ease-in-out;
+				@extend .expansion-card--hover.expansion-card;
 				border: 1px solid $laranja-naruto-light-2;
 				box-shadow: 0 0 2px $laranja-naruto-base;
 			}
 
 			&--yellow {
-				transition: box-shadow .2s ease-in-out;
+				@extend .expansion-card--hover.expansion-card;
 				border: 1px solid $amarelo-pikachu-light-2;
 				box-shadow: 0 0 2px $amarelo-pikachu-base;
 			}
 
 			&--gray {
-				transition: box-shadow .2s ease-in-out;
+				@extend .expansion-card--hover.expansion-card;
 				box-shadow: 0 0 2px $cinza-5;
 			}
 		}
@@ -197,10 +223,97 @@ export default {
 		height: auto;
 	}
 
-	&__expand-icon {
+	&__body {
 		display: flex;
-		align-self: flex-start;
-		margin-left: auto;
+		justify-content: space-between;
+		width: 100%;
+		margin-top: auto;
+		margin-bottom: auto;
+	}
+
+	&__content {
+		display: flex;
+		flex-direction: column;
+	}
+
+	&__main-value {
+		@include titulo-1;
+		color: $cinza-8;
+		margin-bottom: 2px;
+	}
+
+	&__legend {
+		@include legenda;
+		@include margin(onidirecional, 0);
+		color: $cinza-6;
+		max-width: 155px;
+	}
+
+	&__icon {
+		display: flex;
+		padding: 12px;
+		border-radius: 100%;
+		width: 50px;
+		height: 50px;
+		align-self: center;
+		align-items: center;
+		justify-content: center;
+
+		&--blue {
+			color: $azul-sonic-base;
+			background-color: $azul-sonic-light-2;
+		}
+
+		&--turquoise {
+			color: $turquesa-perry-base;
+			background-color: $turquesa-perry-light-2;
+		}
+
+		&--green {
+			color: $verde-piccolo-base;
+			background-color: $verde-piccolo-light-2;
+		}
+
+		&--purple {
+			color: $roxo-thanos-base;
+			background-color: $roxo-thanos-light-2;
+		}
+
+		&--pink {
+			color: $pantera-cor-de-rosa-base;
+			background-color: $pantera-cor-de-rosa-light-2;
+		}
+
+		&--red {
+			color: $vermelho-mario-base;
+			background-color: $vermelho-mario-light-2;
+		}
+
+		&--orange {
+			color: $laranja-naruto-base;
+			background-color: $laranja-naruto-light-2;
+		}
+
+		&--yellow {
+			color: $amarelo-pikachu-base;
+			background-color: $amarelo-pikachu-light-2;
+		}
+
+		&--gray {
+			color: $cinza-5;
+			background-color: $cinza-3;
+		}
+
+		& .b-icon {
+			min-width: 20px;
+			min-height: 20px;
+		}
+	}
+
+	&__expand-icon {
+		position: absolute;
+		top: 8px;
+		right: 12px;
 		color: $cinza-5;
 	}
 
