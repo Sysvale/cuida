f26588471e487dc032852d502cd3ee2759044f57
Patricia Coelho <patriciacoelho.slv@gmail.com>
2020-12-10 13:07:18 -0300

    Add config de largura e altura; e add estilos de variantes;

--- src/components/ExpansionCard.vue
+++ src/components/ExpansionCard.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		class="expansion-card__container"
+		class="expansion-card"
 		:class="expansionCardContainerStyle"
 		@mouseover="!isExpanded ? toggleCardStyle('hover') : null"
 		@mouseleave="!isExpanded ? toggleCardStyle() : null"
@@ -16,10 +16,9 @@
 		<div
 			v-if="isExpanded"
 			class="expansion-card__contract-icon"
+			@click="toggleCardStyle('contract')"
 		>
-			<b-icon-arrows-angle-contract
-				@click="toggleCardStyle('contract')"
-			/>
+			<b-icon-arrows-angle-contract />
 		</div>
 		<!--  -->
 	</div>
@@ -34,7 +33,14 @@ import {
 
 export default {
 	props: {
-		//
+		/**
+		 * A variante da Badge. São 9 variantes: 'turquoise', 'green', 'blue',
+		 * 'purple', 'pink', 'red', 'orange', 'yellow' e 'gray'.
+		 */
+		variant: {
+			type: String,
+			default: 'gray',
+		},
 	},
 
 	components: {
@@ -52,17 +58,41 @@ export default {
 
 	computed: {
 		expansionCardContainerStyle() {
-			let dynamicClass = '';
+			let dynamicClass = this.variantStyle;
 
 			if (this.isHovering) {
-				dynamicClass = 'expansion-card__container--hover';	
+				dynamicClass += ' expansion-card--hover';	
 			}
 			if (this.isExpanded) {
-				dynamicClass = 'expansion-card__container--expanded';	
+				dynamicClass += ' expansion-card--expanded';	
 			}
 
 			return dynamicClass;
-		}
+		},
+
+		variantStyle() {
+			switch (this.variant) {
+				case 'turquoise':
+					return 'expansion-card--turquoise';
+				case 'green':
+					return 'expansion-card--green';
+				case 'blue':
+					return 'expansion-card--blue';
+				case 'purple':
+					return 'expansion-card--purple';
+				case 'pink':
+					return 'expansion-card--pink';
+				case 'red':
+					return 'expansion-card--red';
+				case 'orange':
+					return 'expansion-card--orange';
+				case 'yellow':
+					return 'expansion-card--yellow';
+				case 'gray':
+				default:
+					return 'expansion-card--gray';
+			}
+		},
 	},
 
 	methods: {
@@ -95,22 +125,81 @@ export default {
 	@include padding(onidirecional, 3);
 	border-radius: 16px;
 
-	&__container {
-		@extend .expansion-card;
-		border: 1px solid $cinza-3;
-		min-height: 48px;
+	border: 1px solid $cinza-3;
+	min-height: 48px;
+	width: 255px;
+	height: 130px;
 
-		&--hover {
-			cursor: pointer;
-		}
+	&--hover {
+		transition: transform .2s;
+		transform: scale(1.035);
+		cursor: pointer;
 
-		&--expanded {
-			// 
+		&.expansion-card {
+			&--blue {
+				transition: box-shadow .2s ease-in-out;
+				border: 1px solid $azul-sonic-light-2;
+				box-shadow: 0 0 2px $azul-sonic-base;
+			}
+
+			&--turquoise {
+				transition: box-shadow .2s ease-in-out;
+				border: 1px solid $turquesa-perry-light-2;
+				box-shadow: 0 0 2px $turquesa-perry-base;
+			}
+
+			&--green {
+				transition: box-shadow .2s ease-in-out;
+				border: 1px solid $verde-piccolo-light-2;
+				box-shadow: 0 0 2px $verde-piccolo-base;
+			}
+
+			&--purple {
+				transition: box-shadow .2s ease-in-out;
+				border: 1px solid $roxo-thanos-light-2;
+				box-shadow: 0 0 2px $roxo-thanos-base;
+			}
+
+			&--pink {
+				transition: box-shadow .2s ease-in-out;
+				border: 1px solid $pantera-cor-de-rosa-light-2;
+				box-shadow: 0 0 2px $pantera-cor-de-rosa-base;
+			}
+
+			&--red {
+				transition: box-shadow .2s ease-in-out;
+				border: 1px solid $vermelho-mario-light-2;
+				box-shadow: 0 0 2px $vermelho-mario-base;
+			}
+
+			&--orange {
+				transition: box-shadow .2s ease-in-out;
+				border: 1px solid $laranja-naruto-light-2;
+				box-shadow: 0 0 2px $laranja-naruto-base;
+			}
+
+			&--yellow {
+				transition: box-shadow .2s ease-in-out;
+				border: 1px solid $amarelo-pikachu-light-2;
+				box-shadow: 0 0 2px $amarelo-pikachu-base;
+			}
+
+			&--gray {
+				transition: box-shadow .2s ease-in-out;
+				box-shadow: 0 0 2px $cinza-5;
+			}
 		}
+	}
+
+	&--expanded {
+		transition: all .2s linear;
+		width: 100%;
+		height: auto;
 	}
 
 	&__expand-icon {
 		display: flex;
+		align-self: flex-start;
 		margin-left: auto;
 		color: $cinza-5;
 	}
