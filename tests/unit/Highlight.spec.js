40443186e182b71a05731c8093b6ee18f1be97c0
Rafael Dias <rafaeldias@MacBook-Pro-de-Rafael.local>
2020-12-11 09:07:32 -0300

    Cria testes de estilo

--- tests/unit/Highlight.spec.js
+++ tests/unit/Highlight.spec.js
@@ -11,3 +11,48 @@ test('Component is mounted properly', () => {
 	expect(wrapper).toMatchSnapshot();
 });
 
+describe("Styles based on the variants tests", () => {
+	test("if the the styles are applied properly when the variant is 'info'", () => {
+		const wrapper = mount(Highlight, {
+			localVue,
+			propsData: {
+				variant: 'info',
+			},
+		});
+
+		expect(wrapper.findAll('.highlight__container--info').length).toBe(1);
+	});
+
+	test("if the the styles are applied properly when the variant is 'success'", () => {
+		const wrapper = mount(Highlight, {
+			localVue,
+			propsData: {
+				variant: 'success',
+			},
+		});
+
+		expect(wrapper.findAll('.highlight__container--success').length).toBe(1);
+	});
+
+	test("if the the styles are applied properly when the variant is 'danger'", () => {
+		const wrapper = mount(Highlight, {
+			localVue,
+			propsData: {
+				variant: 'danger',
+			},
+		});
+
+		expect(wrapper.findAll('.highlight__container--danger').length).toBe(1);
+	});
+});
+
+test("if the the styles are applied properly when the highlight is animated", () => {
+	const wrapper = mount(Highlight, {
+		localVue,
+		propsData: {
+			animated: true,
+		},
+	});
+
+	expect(wrapper.findAll('.highlight__container--highlighted').length).toBe(1);
+});
