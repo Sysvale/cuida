1a957d3940e4fda9d2d779a76025233e14ba0caf
Patricia Coelho <patriciacoelho.slv@gmail.com>
2021-03-04 16:42:21 -0300

    Adiciona função de copiar na lista de tokens de tipografia

--- src/docs-components/TypographyList.vue
+++ src/docs-components/TypographyList.vue
@@ -1,9 +1,16 @@
 <template>
 	<div>
+		<copy-token :target="target" />
+
 		<div class="d-flex justify-content-between align-items-center">
 			<div>
-				<div>
-					{{ TypographyTokenName}}
+				<div
+					@click="target = TypographyTokenName"
+					:id="TypographyTokenName"
+					class="copy-clip d-flex align-middle align-items-center"
+				>
+					<copy-icon size="1.1x" class="text-secondary mr-3" />
+					<div class="align-middle"> ${{ TypographyTokenName }} </div>
 				</div>
 				<div>
 					<small class="font-weight-bold mr-4">font-size: {{ fontSize }}px;</small>
@@ -30,7 +37,13 @@
 </template>
 
 <script>
+import { CopyIcon } from 'vue-feather-icons'
+import CopyToken from './CopyToken.vue';
 export default {
+	components: {
+		CopyIcon,
+		CopyToken,
+	},
 	props: {
 		fontSize: {
 			type: Number,
@@ -57,7 +70,11 @@ export default {
 			default: false,
 		},
 	},
-
+	data() {
+		return {
+			target: '',
+		}
+	},
 	computed: {
 		dynamicStyle() {
 			return {
@@ -71,6 +88,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import './../assets/sass/app.scss';
+
 .text-sample {
 	font-size: var(--fontSize);
 	font-weight: var(--fontWeight);
@@ -86,5 +105,28 @@ export default {
 	padding: 8px 20px;
 	border-radius: 4px;
 	background: #FFFFFF;
+}
+
+@each $border-radius-name, $border-radius-token in $border-radius {
+	.#{$border-radius-name} {
+		border-radius: $border-radius-token;
+		width: 60px;
+		height: 60px;
+		background-color: $verde-piccolo-base;
+	}
+}
+
+.copy-clip {
+    width: fit-content;
+    padding: 8px 12px;
+    border-radius: 16px;
+	border: 1px solid $branco;
+	transition: all .15s ease-in-out;
+}
+
+.copy-clip:hover {
+	border: 1px solid $verde-piccolo-light-1;
+	cursor: pointer;
+	transition: all .15s ease-in-out;
 }
 </style>
