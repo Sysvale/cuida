7740062cd8ec8fb4f9cd02e9b0174f602dc9ab94
Hosana <hosanabarceloscandido@gmail.com>
2022-07-19 15:37:49 -0300

    feat: cria componente link

---
+++ src/components/Link.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="link__container">
+		<a :href="href">{{ text }}</a>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		/**
+		*	URL de redirecionamento.
+		*/
+		href: {
+			type: String,
+			required: true,
+		},
+		/**
+		*	Texto utilizado no endereço da URL para o usuário.
+		*/
+		text: {
+        	type: String,
+        	required: true,
+		},
+	},
+};
+</script>
+<style lang="scss">
+@import '../assets/sass/app.scss';
+
+.link__container {
+    display: inline;
+    width: auto;
+    height: 40px;
+    padding: 4px;
+    border-radius: 4px;
+
+    a {
+        font-size: 18px;
+        color: $bn-500;
+    }
+
+    &:hover {
+        background-color: $bn-100;
+    }
+}
+</style>
