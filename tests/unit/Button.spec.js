660d5c1b0110f7683aecfb65f69de6bdc47f49f7
rodrigo.sysvale <rodrigo.barboza@sysvale.com>
2022-07-19 16:18:43 -0300

    feat: corrige teste do button

--- tests/unit/Button.spec.js
+++ tests/unit/Button.spec.js
@@ -6,7 +6,7 @@ import Button from '../../src/components/Button.vue';
 const localVue = createLocalVue();
 localVue.use(BootstrapVue);
 
-test('Component is mounted properly', () => {
+test('Component is mounted properly', async () => {
 	const wrapper = mount(Button, {
 		localVue,
 	});
@@ -20,42 +20,36 @@ test('Component is mounted properly', () => {
 	expect(wrapper).toMatchSnapshot();
 });
 
-describe('Prop variant tests', () => {
-	test('The color variants are properly set', () => {
-		const wrapper = mount(Button, {
-			localVue,
-		});
 
-		wrapper.vm.predefinedColors.forEach((color) => {
-			test(`if the computed property changes when the prop variant is set to '${color}'`, async () => {
-				await wrapper.setProps({ variant: color });
-				expect(wrapper.vm.predefinedStyle).toBe(`badge--${color}`);
-			});
-		});
+describe('The color variants are properly set', () => {
+	const wrapper = mount(Button, {
+		localVue,
 	});
 
-	test('The size variants are properly set', () => {
-		const wrapper = mount(Button, {
-			localVue,
+	wrapper.vm.predefinedColors.forEach((color) => {
+		test(`if the computed property changes when the prop variant is set to '${color}'`, async () => {
+			await wrapper.setProps({ variant: color });
+			expect(wrapper.vm.predefinedColor).toBe(`button--${color}`);
 		});
-
-		wrapper.vm.predefinedSizes.forEach((size) => {
-			test(`if the computed property changes when the prop size is set to '${size}'`, async () => {
-				await wrapper.setProps({ size });
-				expect(wrapper.vm.predefinedStyle).toBe(`badge--${size}`);
-			});
+	});
+	
+	wrapper.vm.predefinedSizes.forEach((size) => {
+		test(`if the computed property changes when the prop size is set to '${size}'`, async () => {
+			await wrapper.setProps({ size });
+			expect(wrapper.vm.predefinedSize).toBe(`button-size--${size}`);
 		});
 	});
 });
 
 test('Button emits an event when clicked', () => {
-	const wrapper = shallowMount(Button, {
+	const wrapper = mount(Button, {
 		localVue,
 	});
 
-	expect(wrapper.find('#cds-button').exists()).toBe(true);
-	wrapper.find('#cds-button').trigger('click');
+	expect(wrapper.find('#cds-button button').exists()).toBe(true);
+	wrapper.find('#cds-button button').trigger('click');
 
-	expect(wrapper.emitted().input).toBeTruthy();
-	expect(wrapper.emitted().input).toEqual([[true]]);
+	console.log(wrapper.emitted()['click']);
+	expect(wrapper.emitted()['click']).toBeTruthy();
+	expect(wrapper.emitted()['click']).toEqual([[true]]);
 })
