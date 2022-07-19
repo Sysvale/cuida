985cb56bd0b00f1f78d081ccf4fd932cb85e0aa7
Tássio Caique <tassiok2@gmail.com>
2022-07-19 13:32:23 -0300

    feat: construção do checkbox com elementos básicos do html

--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -1,26 +1,31 @@
 <template>
 	<span
-		class="checkbox__container"
 		id="cds-checkbox"
+		class="checkbox__container"
+		@click="toggleValue"
 	>
-		<div class="option__checkbox">
+		<div class="checkbox__content">
 			<input
 				v-model="internalValue"
 				type="checkbox"
-				:name="$attrs.name"
+				:id="$attrs.name || 'cds-checkbox-option'"
+				:name="$attrs.name || 'cds-checkbox-option'"
 				:value="true"
 			/>
 			<label
-				:id="`checkbox-${option[label]}`"
-				:for="`input-${option[label]}`"
-				:class="{ 'option__checkbox--checked': option.isSelected }"
-				@click="addItemViaCustomCheckbox(option)"
+				:for="$attrs.name || 'cds-checkbox-option'"
+				:class="{ 'checkbox__content--checked': internalValue }"
+				@click="toggleValue"
 			/>
 		</div>
 		<!-- @slot Slot usado pra mostrar o conteúdo do checkbox. -->
-		<slot>
-			{{ label }}
-		</slot>
+		<div
+			class="checkbox__label"
+		>
+			<slot>
+				{{ label }}
+			</slot>
+		</div>
 	</span>
 </template>
 
@@ -33,20 +38,6 @@ export default {
 		value: {
 			default: null,
 			required: true,
-		},
-		/**
-		 * Prop com o valor a ser utilizado em caso de o checkbox estar marcado
-		 */
-		checkedValue: {
-			type: [Boolean, Object, String],
-			default: true,
-		},
-		/**
-		 * Prop com o valor a ser utilizado em caso de o checkbox não estar marcado
-		 */
-		uncheckedValue: {
-			type: [Boolean, Object, String],
-			default: false,
 		},
 		/**
 		 * Controla o status do grupo de radio buttons
@@ -82,126 +73,94 @@ export default {
 			immediate: true,
 		},
 	},
+
+	methods: {
+		toggleValue() {
+			this.internalValue = !this.internalValue;
+		},
+	}
 };
 </script>
 <style lang="scss">
 @import '../assets/sass/app.scss';
 
 .checkbox__container {
+	display: flex;
+	align-items: center;
+	width: fit-content;
 	cursor: pointer;
 }
 
-#cds-checkbox [type="checkbox"] {
-	width: 18px;
-	position: relative;
-	margin-right: spacer(6);
-	margin-left: spacer(n3);
+#cds-checkbox {
 
-	label {
-		cursor: pointer;
-		position: absolute;
+
+	input[type=checkbox] {
+		visibility: hidden;
+	}
+
+	.checkbox__content {
 		width: 18px;
-		height: 18px;
-		top: 0;
-		border-radius: 4px;
-		border: 1px solid $n-500;
+		position: relative;
+		margin-right: spacer(6);
+		margin-left: spacer(n3);
 
-		&:after {
-			border: 2px solid $n-0;
-			border-top: none;
-			border-right: none;
-			content: "";
-			height: 5px;
-			left: 3.1px;
-			opacity: 0;
+		label {
+			cursor: pointer;
 			position: absolute;
-			top: 4.4px;
-			transform: rotate(-45deg);
-			width: 8px;
-			border-radius: 0.4px;
+			width: 18px;
+			height: 18px;
+			top: 3px;
+			border-radius: $border-radius-button;
+			border: 1px solid $n-500;
+
+			&:after {
+				border: 2px solid $n-0;
+				border-top: none;
+				border-right: none;
+				content: "";
+				height: 6px;
+				width: 12px;
+				left: 3px;
+				opacity: 0;
+				position: absolute;
+				top: 4px;
+				transform: rotate(-45deg);
+				border-radius: 0.4px;
+			}
+		}
+
+		input[type=checkbox]:checked + label:after {
+			-ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=100)";
+			filter: alpha(opacity=100);
+			opacity: 1;
+		}
+
+		input[type="checkbox"]:indeterminate + label:after {
+			-ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=100)";
+			filter: alpha(opacity=100);
+			opacity: 1;
+			border-left: none;
+			top: 4px;
+			width: 9px;
+			transform: rotate(0deg);
 		}
 	}
 
-	&:checked + label:after {
-		-ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=100)";
-		filter: alpha(opacity=100);
+	.checkbox__content--checked {
 		background-color: $gp-500 !important;
-		border-radius: $border-radius-button;
-		opacity: 1;
+		border: none !important;
 	}
 
-	&:focus {
-		box-shadow: 0 0 10px red;
-		outline: none !important;
+	.checkbox__content--indeterminate {
+		background-color: $gp-500 !important;
+		border: none !important;
 	}
 
-	&:indeterminate + label:after {
-		-ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=100)";
-		filter: alpha(opacity=100);
-		opacity: 1;
-		border-left: none;
-		top: 4px;
-		width: 9px;
-		background-color: $gp-500 !important;
-		border-radius: $border-radius-button;
-		transform: rotate(0deg);
+	.checkbox__label {
+		@include body-2;
+		color: $n-500;
 	}
 }
 
-// #cds-checkbox [type="checkbox"] + label {
-// 	@include body-2;
-// 	position: relative;
-// 	padding: pl(6);
-// 	cursor: pointer;
-// 	display: inline-block;
-// 	color: $n-500;
 
-// 	&:after {
-// 		border: 2px solid $n-0;
-// 		border-top: none;
-// 		border-right: none;
-// 		content: "";
-// 		height: 5px;
-// 		left: 3.1px;
-// 		opacity: 0;
-// 		position: absolute;
-// 		top: 4.4px;
-// 		transform: rotate(-45deg);
-// 		width: 8px;
-// 		border-radius: 0.4px;
-// 	}
-// }
-
-// #cds-checkbox [type="checkbox"]:checked + label:before,
-// #cds-checkbox [type="checkbox"]:not(:checked) + label:before {
-// 	position: absolute;
-// 	left: 0;
-// 	top: 0;
-// 	width: 18px;
-// 	height: 18px;
-// 	border: 1px solid $n-500;
-// 	border-radius: $border-radius-button;
-// 	background: $n-0;
-// }
-
-// #cds-checkbox [type="checkbox"]:checked + label:after,
-// #cds-checkbox [type="checkbox"]:not(:checked) + label:after {
-// 	-ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=100)";
-// 	filter: alpha(opacity=100);
-// 	background: $gp-500;
-// 	position: absolute;
-// 	top: 4px;
-// 	left: 4px;
-// 	-webkit-transition: all 0.3s ease;
-// 	transition: all 0.3s ease;
-// 	opacity: 1;
-// 	// -webkit-transform: scale(1);
-// 	// transform: scale(1);
-// }
-
-// #cds-checkbox [type="checkbox"]:not(:checked) + label:after {
-// 	opacity: 0;
-// 	-webkit-transform: scale(0);
-// 	transform: scale(0);
-// }
 </style>
