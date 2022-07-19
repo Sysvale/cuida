de9e0ea45fab9daee7de7cdb0a3a51c80e9ae779
Lucas Nascimento <lucas.barbosa.nas@gmail.com>
2022-07-19 17:00:18 -0300

    feat.: Adiciona variantes de tamanho ao spinner

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -1,7 +1,12 @@
 <template>
 	<div id="cds-spinner">
-		<div class="spinner"/>
-		<div> {{ label }} </div>
+		<div :class="computedSpinnerClass"/>
+		<div
+			v-if="!noText"
+			:class="computedLabelClass"
+		>
+			{{ label }}
+		</div>
 	</div>
 </template>
 
@@ -9,12 +14,35 @@
 export default {
 	props: {
 		/**
-		*	URL da imagem a ser utilizada como loader.
+		*	Texto a ser exibido juntamente com o spinner
 		*/
-		label:
-		{
+		label: {
 			type: String,
-			required: false,
+			default: 'Carregando...',
+		},
+		/**
+		*	Propriedade para inibir a exibição da etiqueta do Spinner
+		*/
+		noText: {
+			type: Boolean,
+			default: false,
+		},
+		/**
+		*	Tamanho do componente
+		*/
+		size: {
+			type: String,
+			default: 'md',
+			validator: (value) => ['sm', 'md', 'lg'].indexOf(value) > -1,
+		},
+	},
+
+	computed: {
+		computedSpinnerClass() {
+			return `spinner--${this.size}`;
+		},
+		computedLabelClass() {
+			return `spinner__label--${this.size}`;
 		},
 	},
 };
@@ -23,20 +51,16 @@ export default {
 <style lang="scss" scoped>
 @import '../assets/sass/app.scss';
 
-$spinner-width: 8px;
+$spinner-width: 15px;
 $spinner-color: #239F78;
 
 .spinner {
 	border-width: $spinner-width;
 	border-style: solid;
 	border-color: $spinner-color $spinner-color $spinner-color transparent;
-	width: 100px;
-	height: 100px;
 	border-radius: 50%;
 	animation: spin 1.5s infinite;
 	position: relative;
-
-	margin: 6em auto;
 
 	&:before, &:after {
 		content: '';
@@ -45,15 +69,77 @@ $spinner-color: #239F78;
 		border-radius: 50%;
 		background: $spinner-color;
 		position: absolute;
-		left: $spinner-width;
+		left: 0.1px;
 	}
 
-	&:before {
-		top: $spinner-width;
+	&--sm {
+		@extend .spinner;
+		width: 40px;
+		height: 40px;
+		border-width: $spinner-width;
+
+		&:before {
+			top: 1px;
+		}
+
+		&:after {
+			bottom: 1px;
+		}
 	}
-	
-	&:after {
-		bottom: $spinner-width;
+
+	&--md {
+		@extend .spinner;
+		width: 80px;
+		height: 80px;
+		$spinner-width: 8px;
+
+		border-width: $spinner-width;
+
+		&:before, &:after {
+			width: $spinner-width;
+			height: $spinner-width;
+			left: 3px;
+		}
+
+		&:before {
+			top: 1px;
+		}
+
+		&:after {
+			bottom: 1px;
+		}
+	}
+
+	&--lg {
+		@extend .spinner;
+		width: 120px;
+		height: 120px;
+		border-width: $spinner-width;
+
+		&:before {
+			top: 1px;
+		}
+
+		&:after {
+			bottom: 1px;
+		}
+	}
+
+	&__label {
+		margin: mt(6);
+		@include subheading-2;
+
+		&--sm {
+			@extend .spinner__label;
+		}
+
+		&--md {
+			@extend .spinner__label;
+		}
+
+		&--lg {
+			@extend .spinner__label;
+		}
 	}
 }
 
