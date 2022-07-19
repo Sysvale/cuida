887cebf8da8d9a2f628769137ded219b101d3b42
Patrícia Coelho <patriciacoelho.slv@gmail.com>
2022-07-19 15:05:25 -0300

    fix: altera suíte de testes do Radio

--- tests/unit/Radio.spec.js
+++ tests/unit/Radio.spec.js
@@ -1,5 +1,6 @@
 import { mount, createLocalVue } from '@vue/test-utils';
 import Radio from '../../src/components/Radio.vue';
+import flushPromises from 'flush-promises';
 
 import BootstrapVue from 'bootstrap-vue';
 const localVue = createLocalVue();
@@ -9,12 +10,31 @@ test('Component is mounted properly', () => {
 	const wrapper = mount(Radio, {
 		localVue,
 		propsData: {
-			value: '',
-			label: 'Option 1',
+			modelValue: null,
+			value: 'option-1',
+			label: 'Opção 1',
+		},
+	});
+
+	expect(wrapper).toMatchSnapshot();
+});
+
+test('if updates v-model with value of radio on click', async () => {
+	let modelValue = null;
+	const wrapper = mount(Radio, {
+		localVue,
+		propsData: {
+			modelValue,
+			value: 'option-1',
+			label: 'Opção 1',
 			disabled: false,
 			invalid: false,
 		},
 	});
 
-	expect(wrapper).toMatchSnapshot();
+	wrapper.find('.cds-radio__content').trigger('click');
+
+	await flushPromises();
+
+	expect(wrapper.emitted().change).toBeTruthy();
 });
