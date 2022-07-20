93854d7e9d94ce6556d83421302dd9b4a709d87d
rodrigo.sysvale <rodrigo.barboza@sysvale.com>
2022-07-20 15:31:29 -0300

    feat: altera o nome do componente no registro

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -114,7 +114,7 @@ Vue.component('tooltip', Tooltip);
 Vue.component('source-code-wrapper', SourceCodeWrapper);
 Vue.component('toggle-switch', ToggleSwitch);
 Vue.component('shadow-builder', Shadows);
-Vue.component('select', Select);
+Vue.component('form-drop-down', Select);
 
 addParameters({
 	options: {
