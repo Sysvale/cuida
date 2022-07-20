942ea7846f7c15ebc7c655bed072c7f4b95c6de8
Uendel Couto <uendel.couto@gmail.com>
2022-07-20 09:35:31 -0300

    feat: testa evento de open e bind de valor do componente

--- tests/unit/Select.spec.js
+++ tests/unit/Select.spec.js
@@ -15,6 +15,45 @@ test('Component is mounted properly', () => {
 	expect(wrapper).toMatchSnapshot();
 });
 
+// evento open
+// select
+// disabled
+// chevron
+
+test('The component emits the open event when it is open', () => {
+	const wrapper = mount(Select, {
+		localVue,
+	});
+
+	expect(wrapper.find('#cds-select').exists()).toBe(true);
+	const select = wrapper.find('#cds-select');
+
+	select.trigger('click');
+
+	expect(wrapper.emitted().open).toBeTruthy();
+});
+
+test('The component makes its value reactive', () => {
+	const wrapper = mount(Select, {
+		localVue,
+		props: {
+			value: 'bar',
+			options: [
+				{
+					'foo': 'foo-value',
+				}
+			],
+		}
+	});
+
+	expect(wrapper.find('select:first-child').exists()).toBe(true);
+	const option = wrapper.find('select:first-child');
+
+	option.trigger('click');
+
+	expect(wrapper.props.value).toBe('foo');
+});
+
 // describe('Prop variant tests', () => {
 // 	const wrapper = mount(Badge, {
 // 		localVue,
