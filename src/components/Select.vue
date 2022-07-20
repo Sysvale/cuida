9c83294d72d9a2a8ccafb2b5d6b4c4ba12c4ad42
RafaelGondi <rdias.ga@gmail.com>
2022-07-20 09:15:04 -0300

    feat: Cria estrutura de arquivos do componente Select

---
+++ src/components/Select.vue
@@ -0,0 +1,140 @@
+<template>
+	<div
+		class="badge__container"
+		:class="predefinedStyle"
+	>
+		<div class="badge__content">
+			<!-- @slot Slot padrão utilizado para exibir o conteúdo dentro da badge -->
+			<slot/>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		/**
+		 * Prop que 
+		 * 'violet', 'pink', 'red', 'orange', 'amber' e 'gray'.
+		 */
+		label: {
+			type: String,
+			default: 'Label',
+		},
+		/**
+		 * A variante da Badge. São 9 variantes: 'turquoise', 'green', 'blue',
+		 * 'violet', 'pink', 'red', 'orange', 'amber' e 'gray'.
+		 */
+		placeholder: {
+			type: String,
+			default: 'Selecione...',
+		},
+		disabled: {
+			type: String,
+			default: 'Selecione...',
+		},
+	},
+
+	data() {
+		return {
+			predefinedColors: [
+				'turquoise',
+				'green',
+				'blue',
+				'indigo',
+				'violet',
+				'pink',
+				'red',
+				'orange',
+				'amber',
+				'gray',
+			],
+		};
+	},
+
+	computed: {
+		predefinedStyle() {
+			if (this.predefinedColors.indexOf(this.variant) > -1) {
+				return `badge--${this.variant}`;
+			}
+			return 'badge--gray';
+		},
+	},
+};
+</script>
+<style lang="scss" scoped>
+@import '../assets/sass/app.scss';
+
+.badge {
+	&__container {
+		border-radius: 50px !important;
+		width: fit-content;
+		padding: pYX(0, 2);
+	}
+
+	&__content {
+		font-weight: 500 !important;
+		@include body-2;
+	}
+
+	&--turquoise {
+		color: $ts-700;
+		background-color: $ts-100;
+		border: 1px solid $ts-400;
+	}
+
+	&--green {
+		color: $gp-700;
+		background-color: $gp-100;
+		border: 1px solid $gp-400;
+	}
+
+	&--blue {
+		color: $bn-700;
+		background-color: $bn-100;
+		border: 1px solid $bn-400;
+	}
+
+	&--indigo {
+		color: $in-700;
+		background-color: $in-100;
+		border: 1px solid $in-400;
+	}
+
+	&--violet {
+		color: $vr-700;
+		background-color: $vr-100;
+		border: 1px solid $vr-400;
+	}
+
+	&--pink {
+		color: $pp-700;
+		background-color: $pp-100;
+		border: 1px solid $pp-400;
+	}
+
+	&--red {
+		color: $rc-700;
+		background-color: $rc-100;
+		border: 1px solid $rc-400;
+	}
+
+	&--orange {
+		color: $og-700;
+		background-color: $og-100;
+		border: 1px solid $og-400;
+	}
+
+	&--amber {
+		color: $al-700;
+		background-color: $al-100;
+		border: 1px solid $al-500;
+	}
+
+	&--gray {
+		color: $n-700;
+		background-color: $n-20;
+		border: 1px solid $n-200;
+	}
+}
+</style>
---
+++ tests/unit/Select.spec.js
@@ -0,0 +1,32 @@
+import { mount, createLocalVue } from '@vue/test-utils';
+import BootstrapVue from 'bootstrap-vue';
+import Select from '../../src/components/Select.vue';
+
+const localVue = createLocalVue();
+localVue.use(BootstrapVue);
+
+test('Component is mounted properly', () => {
+	const wrapper = mount(Select, {
+		localVue,
+		slots: {
+			default: 'Conteúdo',
+		},
+	});
+	expect(wrapper).toMatchSnapshot();
+});
+
+// describe('Prop variant tests', () => {
+// 	const wrapper = mount(Badge, {
+// 		localVue,
+// 		slots: {
+// 			default: 'Conteúdo',
+// 		},
+// 	});
+
+// 	wrapper.vm.predefinedColors.forEach((color) => {
+// 		test(`if the computed property changes when the prop variant is setted to '${color}'`, async () => {
+// 			await wrapper.setProps({ variant: color });
+// 			expect(wrapper.vm.predefinedStyle).toBe(`badge--${color}`);
+// 		});
+// 	});
+// });
