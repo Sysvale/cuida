a85b4edfaf76bcdc5353a29dc745b634d85203ea
RafaelGondi <rdias.ga@gmail.com>
2022-07-21 11:01:47 -0300

    feat: Adiciona chevron ao select

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -2,19 +2,26 @@
 	<div
 		id="cds-select"
 	>
-		<span>{{ label }}</span>
-		<select
-			:disabled="disabled"
-			v-model="selected"
-			class="select__container"
+		<span
+			class="select__label"
 		>
-			<option
-				v-for="option in options"
-				:key="option.value"
-			>
-				{{ option.value }}
-			</option>
-		</select>
+			{{ label }}
+		</span>
+
+		<div class="select__container">
+			<select class="select-dropdown">
+				<option selected disabled hidden>
+					{{ placeholder }}
+				</option>
+				<option
+					v-for="option in options"
+					:key="option.value"
+				>
+					{{ option.value }}
+				</option>
+			</select>
+			<span class="custom-arrow"></span>
+		</div>
 	</div>
 </template>
 
@@ -87,82 +94,59 @@ export default {
 <style lang="scss" scoped>
 @import '../assets/sass/app.scss';
 
-#cds-select {
-	display: flex;
-	flex-direction: column;
-}
 .select {
-	&__container {
+	&-dropdown {
 		border-radius: $border-radius-extra-small;
-		width: fit-content !important;
+		max-width: 576px;
 		height: 40px;
-		border: 1px solid $n-40;
+		border: 1px solid $n-50;
 		background: $n-0;
+		padding-right: spacer(5);
+		padding-left: spacer(3);
+		color: $n-600;
 	}
 
-	&__content {
-		font-weight: 500 !important;
-		@include body-2;
-	}
-
-	&--turquoise {
-		color: $ts-700;
-		background-color: $ts-100;
-		border: 1px solid $ts-400;
-	}
-
-	&--green {
-		color: $gp-700;
-		background-color: $gp-100;
-		border: 1px solid $gp-400;
-	}
-
-	&--blue {
-		color: $bn-700;
-		background-color: $bn-100;
-		border: 1px solid $bn-400;
-	}
-
-	&--indigo {
-		color: $in-700;
-		background-color: $in-100;
-		border: 1px solid $in-400;
-	}
-
-	&--violet {
-		color: $vr-700;
-		background-color: $vr-100;
-		border: 1px solid $vr-400;
-	}
-
-	&--pink {
-		color: $pp-700;
-		background-color: $pp-100;
-		border: 1px solid $pp-400;
-	}
-
-	&--red {
-		color: $rc-700;
-		background-color: $rc-100;
-		border: 1px solid $rc-400;
-	}
-
-	&--orange {
-		color: $og-700;
-		background-color: $og-100;
-		border: 1px solid $og-400;
-	}
-
-	&--amber {
-		color: $al-700;
-		background-color: $al-100;
-		border: 1px solid $al-500;
-	}
-
-	&--gray {
+	&__label {
+		font-weight: $font-weight-semibold;
 		color: $n-700;
-		background-color: $n-20;
-		border: 1px solid $n-200;
+		font-size: 14px;
+		margin: mb(1);
 	}
+
+	&__container {
+		width: fit-content;
+		position: relative;
+	}
+}
+
+.custom-arrow {
+	position: absolute;
+	top: 2px;
+	right: 2px;
+	display: block;
+	background: $n-0;
+	height: 36px;
+	width: 36px;
+	pointer-events: none;
+	border-radius: $border-radius-small;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+
+.custom-arrow::before,
+.custom-arrow::after {
+	content: '';
+	width: 0;
+	height: 0;
+}
+
+.custom-arrow::before {
+	border-left: 6px solid transparent;
+	border-right: 6px solid transparent;
+	border-bottom: 6px solid $n-100;
+	border-radius: $border-radius-small;
+	transition: all 0.25s ease-in-out;
+	transform: rotate(180deg);
 }
 </style>
