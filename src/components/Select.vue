3d01b21decd8f0c239ce9b871915bfbb49f46d17
RafaelGondi <rdias.ga@gmail.com>
2022-07-27 10:11:39 -0300

    feat: inicia estilização do select

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -1,6 +1,7 @@
 <template>
 	<div
 		id="cds-select"
+		class="select"
 	>
 		<span
 			class="select__label"
@@ -8,24 +9,43 @@
 			{{ label }}
 		</span>
 
-		<div class="select__container">
-			<select class="select-dropdown">
-				<option selected disabled hidden>
-					{{ placeholder }}
-				</option>
-				<option
+		<div
+			class="select__container"
+		>
+			<input
+				v-on-click-outside="hide"
+				class="select__input"
+				type="text"
+				@click="activeSelection"
+				onkeypress="return false;"
+				:placeholder="placeholder"
+			/>
+
+			<div
+				v-if="active"
+				class="select__options"
+			>
+				<div
 					v-for="option in options"
 					:key="option.value"
+					class="option__text"
 				>
-					{{ option.value }}
-				</option>
-			</select>
-			<span class="custom-arrow"></span>
+					<span>
+						{{ option.value }}
+					</span>
+				</div>
+			</div>
+
+			<span
+				:class="active ? 'select__chevron--opened' : 'select__chevron--closed'"
+			/>
 		</div>
 	</div>
 </template>
 
 <script>
+import { directive as onClickOutside } from 'vue-on-click-outside';
+
 export default {
 	props: {
 		/**
@@ -69,10 +89,20 @@ export default {
 		},
 	},
 
+	directives: {
+		'on-click-outside': onClickOutside,
+	},
+
 	data() {
 		return {
 			selected: this.value,
+			active: false,
+			id: null,
 		};
+	},
+
+	mounted() {
+		this.id = `filter-pill$-${this._uid}`;
 	},
 
 	computed: {
@@ -81,6 +111,27 @@ export default {
 				return `badge--${this.variant}`;
 			}
 			return 'badge--gray';
+		},
+	},
+
+	methods: {
+		activeSelection() {
+			if (this.disabled) {
+				return;
+			}
+
+			this.active = !this.active;
+
+			/**
+			* Evento que indica que o Select foi clicado
+			* @event click
+			* @type {Event}
+			*/
+			this.$emit('click', true);
+		},
+
+		hide() {
+			this.active = false;
 		},
 	},
 
@@ -95,15 +146,23 @@ export default {
 @import '../assets/sass/app.scss';
 
 .select {
-	&-dropdown {
+	&__input {
 		border-radius: $border-radius-extra-small;
 		max-width: 576px;
+		min-width: 120px;
 		height: 40px;
 		border: 1px solid $n-50;
 		background: $n-0;
 		padding-right: spacer(5);
 		padding-left: spacer(3);
 		color: $n-600;
+		caret-color: transparent;
+		cursor: pointer;
+		background-color: $n-0;
+
+		&:focus-visible {
+			outline: none;
+		}
 	}
 
 	&__label {
@@ -117,36 +176,87 @@ export default {
 		width: fit-content;
 		position: relative;
 	}
+
+	&__chevron--closed {
+		position: absolute;
+		top: 6px;
+		right: 2px;
+		display: block;
+		background: $n-0;
+		height: 32px;
+		width: 32px;
+		pointer-events: none;
+		border-radius: $border-radius-small;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		&::before,
+		&::after {
+			content: '';
+			width: 0;
+			height: 0;
+		}
+
+		&::before {
+			border-left: 6px solid transparent;
+			border-right: 6px solid transparent;
+			border-bottom: 6px solid $n-100;
+			border-radius: $border-radius-small;
+			transition: all 300ms ease-in-out;
+			transform: rotate(-180deg);
+		}
+	}
+
+	&__chevron--opened {
+		position: absolute;
+		top: 6px;
+		right: 2px;
+		display: block;
+		background: $n-0;
+		height: 32px;
+		width: 32px;
+		pointer-events: none;
+		border-radius: $border-radius-small;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		&::before,
+		&::after {
+			content: '';
+			width: 0;
+			height: 0;
+		}
+
+		&::before {
+			border-left: 6px solid transparent;
+			border-right: 6px solid transparent;
+			border-bottom: 6px solid $n-100;
+			border-radius: $border-radius-small;
+			transition: all 300ms ease-in-out;
+			transform: rotate(0deg);
+		}
+	}
+
+	&__options {
+		outline: 1px solid $n-50;
+		border-radius: $border-radius-extra-small;
+		display: flex;
+		flex-direction: column;
+		justify-items: center;
+	}
+
 }
 
-.custom-arrow {
-	position: absolute;
-	top: 2px;
-	right: 2px;
-	display: block;
-	background: $n-0;
-	height: 36px;
-	width: 36px;
-	pointer-events: none;
-	border-radius: $border-radius-small;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-}
+.option {
+	&__text {
+		padding: pYX(2, 3);
 
-.custom-arrow::before,
-.custom-arrow::after {
-	content: '';
-	width: 0;
-	height: 0;
-}
-
-.custom-arrow::before {
-	border-left: 6px solid transparent;
-	border-right: 6px solid transparent;
-	border-bottom: 6px solid $n-100;
-	border-radius: $border-radius-small;
-	transition: all 0.25s ease-in-out;
-	transform: rotate(180deg);
+		&:hover {
+			background-color: $n-20;
+			cursor: pointer;
+		}
+	}
 }
 </style>
