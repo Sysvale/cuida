3eda021fc3d8f83ad6a19e3602825eff133dadf9
RafaelGondi <rdias.ga@gmail.com>
2022-07-28 14:12:34 -0300

    feat: cria o método removeAccest na pasta utils

---
+++ src/utils/methods/removeAccents.js
@@ -0,0 +1,21 @@
+export default function removeAccents(str = '') {
+	const accents = 'ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž';
+	const accentsOut = 'AAAAAAaaaaaaOOOOOOOooooooEEEEeeeeeCcDIIIIiiiiUUUUuuuuNnSsYyyZz';
+	const strLen = str.length;
+	let newStr = str.split('');
+	let x;
+
+	for (let i = 0; i < strLen; i += 1) {
+		x = accents.indexOf(str[i]);
+		if (x !== -1) {
+			newStr[i] = accentsOut[x];
+		}
+	}
+
+	newStr = newStr.join('');
+	newStr = newStr.split('.').join('');
+	newStr = newStr.split('-').join('');
+	newStr = newStr.split('/').join('');
+
+	return newStr;
+}
