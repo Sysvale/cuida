cbf62d63235a8e2d399164991e59c89635adffc4
RafaelGondi <rdias.ga@gmail.com>
2022-08-08 16:28:42 -0300

    feat: adiciona suporte a sass

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -4,6 +4,7 @@ module.exports = {
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
+    '@storybook/preset-scss',
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
