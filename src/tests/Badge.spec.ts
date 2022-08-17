01f2f1d87b41f7993f2888626ba2fb6aa59a10da
Lucas Nascimento <lucas.barbosa.nas@gmail.com>
2022-08-16 22:34:25 -0300

    fix: Ajusta código do teste

--- src/tests/Badge.spec.ts
+++ src/tests/Badge.spec.ts
@@ -1,10 +1,11 @@
-import { describe, test, expect } from 'vitest';
+// @vitest-environment jsdom
+import { describe, it, expect } from 'vitest';
 import { flushPromises } from '@vue/test-utils';
 import Badge from '../components/Badge.vue';
 import { mount } from '@vue/test-utils';
 
 describe('Badge', () => {
-    test('renders correctly', async () => {
+    it('renders correctly', async () => {
         const wrapper = mount(Badge, {
             variant: 'gray',
         });
