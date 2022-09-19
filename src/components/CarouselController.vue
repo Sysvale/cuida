8f85eaa0d9f0a488d31301f7c365dcd81dfded7c
RafaelGondi <rdias.ga@gmail.com>
2022-09-19 08:24:36 -0300

    fix: Corrige problema nos botões

--- src/components/CarouselController.vue
+++ src/components/CarouselController.vue
@@ -6,144 +6,139 @@
 		>
 			<span
 				:class="{
-					'carousel-controller__previous-button--disabled': disabledback,
-					'carousel-controller__previous-button': !disabledback
+					'carousel-controller__previous-button--disabled': disabledBack,
+					'carousel-controller__previous-button': !disabledBack
 				}"
 				@click="handleClickBack"
 			/>
 
 			<div>
-				<span v-if="!simple" class="text">
+				<span v-if="!simple" class="carousel-controller__text">
 					{{ currentNumberOfItemsMin }}-{{ currentNumberOfItemsMax }} de {{ total }}
 				</span>
-				<span v-else class="text">
+				<span v-else class="carousel-controller__text">
 					{{ currentNumberOfItemsMin }} de {{ numberOfPages }}
 				</span>
 	
-				<span class="value"> {{ propertyName }} </span>
+				<span class="carousel-controller__property"> {{ propertyName }} </span>
 			</div>
 
 			<span
-				@click="handleClickForward"
 				:class="{
 					'carousel-controller__next-button--disabled': disabledForward,
 					'carousel-controller__next-button': !disabledForward
 				}"
+				@click="handleClickForward"
 			/>
 		</div>
 	</div>
 </template>
 
 <script>
-	export default {
-		props: {
-			propertyName: {
-				type: String,
-				default: 'Dados',
-				required: false,
-			},
-
-			perPage: {
-				type: Number,
-				required: true,
-			},
-
-			total: {
-				type: Number,
-				required: true,
-			},
-
-			simple: {
-				type: Boolean,
-				default: false,
-				required: false,
-			},
-
-			fluid: {
-				type: Boolean,
-				default: false,
-				required: false,
-			},
+export default {
+	props: {
+		propertyName: {
+			type: String,
+			default: 'Dados',
+			required: false,
 		},
 
-		data() {
-			return {
-				currentNumberOfItemsMin: 1,
-				currentNumberOfItemsMax: this.perPage,
-			};
+		perPage: {
+			type: Number,
+			required: true,
 		},
 
-		computed: {
-			numberOfPages() {
-				return Math.ceil(this.total / this.perPage);
-			},
+		total: {
+			type: Number,
+			required: true,
+		},
 
-			disabledForward() {
-				return (this.currentNumberOfItemsMax >= this.total) 
-				|| (this.simple && (this.currentNumberOfItemsMin >= this.numberOfPages));
-			},
+		simple: {
+			type: Boolean,
+			default: false,
+			required: false,
+		},
 
-			disabledback() {
-				return this.currentNumberOfItemsMin === 1;
+		fluid: {
+			type: Boolean,
+			default: false,
+			required: false,
+		},
+	},
+
+	data() {
+		return {
+			currentNumberOfItemsMin: 1,
+			currentNumberOfItemsMax: this.perPage,
+		};
+	},
+
+	computed: {
+		numberOfPages() {
+			return Math.ceil(this.total / this.perPage);
+		},
+
+		disabledForward() {
+			return (this.currentNumberOfItemsMax >= this.total) 
+			|| (this.simple && (this.currentNumberOfItemsMin >= this.numberOfPages));
+		},
+
+		disabledBack() {
+			return this.currentNumberOfItemsMin === 1;
+		}
+	},
+
+	methods: {
+		handleClickBack() {
+			if (this.disabledBack) return;
+
+			if (this.simple && (this.currentNumberOfItemsMin > 1)) {
+				this.currentNumberOfItemsMin -= 1;
+				this.$emit('click-back', { min: this.currentNumberOfItemsMin - 1, max: this.currentNumberOfItemsMax + 1 });
+				return;
+			} else if (this.simple && (this.currentNumberOfItemsMin === 1)) {
+				return;
+			}
+
+			if (this.currentNumberOfItemsMin - this.perPage <= 0) {
+				this.currentNumberOfItemsMin = 1;
+				this.currentNumberOfItemsMax = this.perPage;
+				this.$emit('click-back', { min: this.currentNumberOfItemsMin - 1, max: this.currentNumberOfItemsMax + 1 });
+			} else {
+				this.currentNumberOfItemsMax -= this.currentNumberOfItemsMax - this.currentNumberOfItemsMin + 1;
+				this.currentNumberOfItemsMin -= this.perPage;
+				this.$emit('click-back', { min: this.currentNumberOfItemsMin - 1, max: this.currentNumberOfItemsMax});
 			}
 		},
 
-		methods: {
-			handleClickBack() {
-				if (this.simple && (this.currentNumberOfItemsMin > 1)) {
-					this.currentNumberOfItemsMin -= 1;
-					this.$emit('click-back');
-					return;
-				} else if (this.simple && (this.currentNumberOfItemsMin === 1)) {
-					return;
-				}
+		handleClickForward() {
+			if (this.disabledForward) return;
 
-				if (this.currentNumberOfItemsMin - this.perPage <= 0) {
-					this.currentNumberOfItemsMin = 1;
-					this.currentNumberOfItemsMax = this.perPage;
-				} else {
-					this.currentNumberOfItemsMin -= this.perPage;
-					this.currentNumberOfItemsMax -= this.perPage;
-					this.$emit('click-back');
-				}
-			},
+			if (this.simple && (this.currentNumberOfItemsMin < this.numberOfPages)) {
+				this.currentNumberOfItemsMin += 1;
+				this.$emit('click-forward', { min: this.currentNumberOfItemsMin - 1, max: this.currentNumberOfItemsMax + 1 });
+				return;
+			} else if (this.simple && (this.currentNumberOfItemsMin === this.numberOfPages)) {
+				return;
+			}
 
-			handleClickForward() {
-				if (this.simple && (this.currentNumberOfItemsMin < this.numberOfPages)) {
-					this.currentNumberOfItemsMin += 1;
-					this.$emit('click-forward');
-					return;
-				} else if (this.simple && (this.currentNumberOfItemsMin === this.numberOfPages)) {
-					return;
-				}
-
-				if ((this.currentNumberOfItemsMax + this.perPage) > this.total) {
-					this.currentNumberOfItemsMax = this.total;
-				} else {
-					this.currentNumberOfItemsMin = this.currentNumberOfItemsMax;
-					this.currentNumberOfItemsMax += this.perPage;
-					this.$emit('click-forward');
-				}
-			},
+			if ((this.currentNumberOfItemsMax + this.perPage) > this.total) {
+				this.currentNumberOfItemsMin = this.currentNumberOfItemsMax + 1;
+				this.currentNumberOfItemsMax = this.total;
+				this.$emit('click-forward', { min: this.currentNumberOfItemsMin - 1, max: this.currentNumberOfItemsMax + 1 });
+			} else {
+				this.currentNumberOfItemsMin = this.currentNumberOfItemsMax + 1;
+				this.currentNumberOfItemsMax += this.perPage;
+				this.$emit('click-forward', { min: this.currentNumberOfItemsMin - 1, max: this.currentNumberOfItemsMax + 1 });
+			}
 		},
-	}
+	},
+}
 </script>
 
 <style lang="scss" scoped>
 @import '../assets/sass/app.scss';
-.text {
-	margin: ml(7);
-	font-size: 14px;
-	color: $n-500;
-	font-weight: $font-weight-semibold;
-}
 
-.value {
-	margin: mTRBL(0, 7, 0, 2);
-	font-size: 14px;
-	color: $n-800;
-	font-weight: $font-weight-semibold;
-}
 .carousel-controller {
 	position: relative;
 	width: fit-content;
@@ -154,112 +149,113 @@
 		width: 100%;
 		justify-content: space-between;
 	}
-}
-.carousel-controller__previous-button {
-	display: block;
-	height: 32px;
-	width: 32px;
-	cursor: pointer;
-	border-radius: $border-radius-small;
-	display: flex;
-	align-items: center;
-	justify-content: center;
 
-	&::before,
-	&::after {
-		content: '';
-		width: 0;
-		height: 0;
+	&__text {
+		margin: ml(7);
+		font-size: 14px;
+		color: $n-500;
+		font-weight: $font-weight-semibold;
 	}
 
-	&::before {
-		border-left: 6px solid transparent;
-		border-right: 6px solid transparent;
-		border-bottom: 6px solid $n-200;
+	&__property {
+		margin: mTRBL(0, 7, 0, 2);
+		font-size: 14px;
+		color: $n-800;
+		font-weight: $font-weight-semibold;
+	}
+
+	&__previous-button {
+		display: block;
+		height: 32px;
+		width: 32px;
+		cursor: pointer;
 		border-radius: $border-radius-small;
-		transition: all 100ms ease-in-out;
-		transform: rotate(-90deg);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	
+		&::before,
+		&::after {
+			content: '';
+			width: 0;
+			height: 0;
+		}
+	
+		&::before {
+			border-left: 6px solid transparent;
+			border-right: 6px solid transparent;
+			border-bottom: 6px solid $n-400;
+			border-radius: $border-radius-small;
+			transition: all 100ms ease-in-out;
+			transform: rotate(-90deg);
+		}
+
+		&--disabled {
+			@extend .carousel-controller__previous-button;
+
+			&::before,
+			&::after {
+				content: '';
+				width: 0;
+				height: 0;
+			}
+	
+			&::before {
+				border-left: 6px solid transparent;
+				border-right: 6px solid transparent;
+				border-bottom: 6px solid $n-100;
+				border-radius: $border-radius-small;
+				transition: all 100ms ease-in-out;
+				transform: rotate(-90deg);
+			}
+		}
 	}
-}
-
-.carousel-controller__previous-button--disabled {
-	display: block;
-	height: 32px;
-	width: 32px;
-	cursor: default;
-	border-radius: $border-radius-small;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-
-	&::before,
-	&::after {
-		content: '';
-		width: 0;
-		height: 0;
-	}
-
-	&::before {
-		border-left: 6px solid transparent;
-		border-right: 6px solid transparent;
-		border-bottom: 6px solid $n-40;
+	
+	&__next-button {
+		display: block;
+		height: 32px;
+		width: 32px;
+		cursor: pointer;
 		border-radius: $border-radius-small;
-		transition: all 100ms ease-in-out;
-		transform: rotate(-90deg);
-	}
-}
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	
+		&::before,
+		&::after {
+			content: '';
+			width: 0;
+			height: 0;
+		}
+	
+		&::before {
+			border-left: 6px solid transparent;
+			border-right: 6px solid transparent;
+			border-bottom: 6px solid $n-400;
+			border-radius: $border-radius-small;
+			transition: all 100ms ease-in-out;
+			transform: rotate(90deg);
+		}
 
-.carousel-controller__next-button {
-	display: block;
-	height: 32px;
-	width: 32px;
-	cursor: pointer;
-	border-radius: $border-radius-small;
-	display: flex;
-	align-items: center;
-	justify-content: center;
+		&--disabled {
+			@extend .carousel-controller__next-button;
 
-	&::before,
-	&::after {
-		content: '';
-		width: 0;
-		height: 0;
-	}
-
-	&::before {
-		border-left: 6px solid transparent;
-		border-right: 6px solid transparent;
-		border-bottom: 6px solid $n-200;
-		border-radius: $border-radius-small;
-		transition: all 100ms ease-in-out;
-		transform: rotate(90deg);
-	}
-}
-
-.carousel-controller__next-button--disabled {
-	display: block;
-	height: 32px;
-	width: 32px;
-	cursor: default;
-	border-radius: $border-radius-small;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-
-	&::before,
-	&::after {
-		content: '';
-		width: 0;
-		height: 0;
-	}
-
-	&::before {
-		border-left: 6px solid transparent;
-		border-right: 6px solid transparent;
-		border-bottom: 6px solid $n-40;
-		border-radius: $border-radius-small;
-		transition: all 100ms ease-in-out;
-		transform: rotate(90deg);
+			&::before,
+			&::after {
+				content: '';
+				width: 0;
+				height: 0;
+			}
+		
+			&::before {
+				border-left: 6px solid transparent;
+				border-right: 6px solid transparent;
+				border-bottom: 6px solid $n-100;
+				border-radius: $border-radius-small;
+				transition: all 100ms ease-in-out;
+				transform: rotate(90deg);
+			}
+		}
 	}
 }
 </style>
