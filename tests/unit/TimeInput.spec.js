f68af7b40ba9fb32e9a5e665959afdde8a1f85bc
Lucas Nascimento <lucas.barbosa.nas@gmail.com>
2022-09-26 15:05:29 -0300

    feat: Cria teste de emissão de evento para modo range

--- tests/unit/TimeInput.spec.js
+++ tests/unit/TimeInput.spec.js
@@ -1,7 +1,6 @@
 import { createLocalVue, shallowMount } from '@vue/test-utils';
 import flushPromises from 'flush-promises';
 import TimeInput from '../../src/components/TimeInput.vue';
-import { colorOptions } from '../../src/utils/constants/colors';
 
 const localVue = createLocalVue();
 
@@ -28,7 +27,7 @@ describe('TimeInput', () => {
 		expect(valueValidator('wrong')).toBeFalsy();
 	});
 
-	test('if input event is emitted correctly', async () => {
+	test('if input event is emitted correctly for single mode', async () => {
 		const wrapper = shallowMount(TimeInput, {
 			localVue,
 			propsData: {
@@ -47,6 +46,36 @@ describe('TimeInput', () => {
 		await inputArray[1].setValue(35);
 
 		expect(wrapper.emitted().input[0]).toContain('15:35');
+	});
+
+	test('if input event is emitted correctly for range mode', async () => {
+		const wrapper = shallowMount(TimeInput, {
+			localVue,
+			propsData: {
+				label: 'Test label',
+				mode: 'range',
+			},
+		});
+
+		await flushPromises();
+
+		const inputArray = wrapper.findAll('input').wrappers;
+
+		await inputArray[0].setValue(15);
+
+		expect(wrapper.emitted().input).toBeFalsy();
+
+		await inputArray[1].setValue(35);
+
+		expect(wrapper.emitted().input).toBeFalsy();
+
+		await inputArray[2].setValue(16);
+
+		expect(wrapper.emitted().input).toBeFalsy();
+
+		await inputArray[3].setValue(35);
+
+		expect(wrapper.emitted().input[0]).toContainEqual(['15:35', '16:35']);
 	});
 
 	test('if wrong input value is converted to correct time range on event emission', async () => {
