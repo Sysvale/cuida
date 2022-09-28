b76a94cd3643a04a3f618f85da2658a1c8accd18
Lucas Nascimento <lucas.barbosa.nas@gmail.com>
2022-09-28 16:34:03 -0300

    fix: Ajusta testes para corresponderem à nova lógica do componente

--- tests/unit/TimeInput.spec.js
+++ tests/unit/TimeInput.spec.js
@@ -19,12 +19,14 @@ describe('TimeInput', () => {
 	});
 
 	test('if "value" prop is validated correctly', async () => {
-		expect.assertions(2);
+		expect.assertions(4);
 
 		const valueValidator = TimeInput.props.value.validator;
 
 		expect(valueValidator('15:15')).toBeTruthy();
+		expect(valueValidator(['13:15', '14:20'])).toBeTruthy();
 		expect(valueValidator('wrong')).toBeFalsy();
+		expect(valueValidator(['15:20', '11:10'])).toBeFalsy();
 	});
 
 	test('if input event is emitted correctly for single mode', async () => {
@@ -41,11 +43,11 @@ describe('TimeInput', () => {
 
 		await inputArray[0].setValue(15);
 
-		expect(wrapper.emitted().input).toBeFalsy();
+		expect(wrapper.emitted().input[0]).toContain(null);
 
 		await inputArray[1].setValue(35);
 
-		expect(wrapper.emitted().input[0]).toContain('15:35');
+		expect(wrapper.emitted().input[1]).toContain('15:35');
 	});
 
 	test('if input event is emitted correctly for range mode', async () => {
@@ -63,19 +65,19 @@ describe('TimeInput', () => {
 
 		await inputArray[0].setValue(15);
 
-		expect(wrapper.emitted().input).toBeFalsy();
+		expect(wrapper.emitted().input[0]).toContain(null);
 
 		await inputArray[1].setValue(35);
 
-		expect(wrapper.emitted().input).toBeFalsy();
+		expect(wrapper.emitted().input[1]).toContain(null);
 
 		await inputArray[2].setValue(16);
 
-		expect(wrapper.emitted().input).toBeFalsy();
+		expect(wrapper.emitted().input[2]).toContain(null);
 
 		await inputArray[3].setValue(35);
 
-		expect(wrapper.emitted().input[0]).toContainEqual(['15:35', '16:35']);
+		expect(wrapper.emitted().input[3]).toContainEqual(['15:35', '16:35']);
 	});
 
 	test('if wrong input value is converted to correct time range on event emission', async () => {
@@ -92,10 +94,10 @@ describe('TimeInput', () => {
 
 		await inputArray[0].setValue(25.3);
 
-		expect(wrapper.emitted().input).toBeFalsy();
+		expect(wrapper.emitted().input[0]).toContain(null);
 
 		await inputArray[1].setValue(-12);
 
-		expect(wrapper.emitted().input[0]).toContain('23:00');
+		expect(wrapper.emitted().input[1]).toContain('23:00');
 	});
 });
