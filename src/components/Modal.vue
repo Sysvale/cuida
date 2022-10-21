4e0811a51607428a4ae6b8c972a858bba6fe344d
Patrícia Coelho <patriciacoelho.slv@gmail.com>
2022-10-21 12:10:16 -0300

    feat: define eventos do modal; e estiliza backdrop e modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,12 +1,52 @@
 <template>
 	<div
 		v-if="internalShow"
-		class="cds-modal"
-		:style="dynamicStyle"
-		v-on-click-outside="hide"
+		class="cds-modal__backdrop"
 	>
-		<!-- @slot Slot usado para inserção de conteúdo dentro do Modal. -->
-		<slot />
+		<div
+			v-if="internalShow"
+			class="cds-modal"
+			:style="dynamicStyle"
+			v-on-click-outside="close"
+		>
+			<header>
+				<!-- @slot Slot usado para utilização de header customizado. -->
+				<slot name="header">
+					<div class="cds-modal__header">
+						<h3>Título</h3>
+						<ion-icon
+							name="close-outline"
+							class="cds-modal__close-icon"
+							@click="close"
+						/>
+					</div>
+				</slot>
+			</header>
+
+			<!-- @slot Slot usado para inserção de conteúdo dentro do Modal. -->
+			<section>
+				<slot />
+			</section>
+
+			<footer class="cds-modal__footer">
+				<!-- @slot Slot usado para inserção de footer customizado. -->
+				<slot name="footer">
+					<cds-button
+						text="Cancelar"
+						secondary
+						:disabled="disabled"
+						@click="close"
+					/>
+					<cds-button
+						class="ml-2"
+						text="Ok"
+						variant="green"
+						:disabled="disabled"
+						@click="ok"
+					/>
+				</slot>
+			</footer>
+		</div>
 	</div>
 </template>
 
@@ -33,13 +73,6 @@ export default {
 			required: true,
 		},
 		/**
-		 * Controla o posicionamento do modal horizontalmente.
-		 */
-		right: {
-			type: Number,
-			default: 0,
-		},
-		/**
 		 * Controla o posicionamento do modal verticalmente.
 		 */
 		top: {
@@ -51,7 +84,7 @@ export default {
 		 */
 		height: {
 			type: Number,
-			default: 100,
+			default: 150,
 		},
 		/**
 		 * Define a largura do modal.
@@ -59,6 +92,13 @@ export default {
 		width: {
 			type: Number,
 			default: 150,
+		},
+		/**
+		 * Define a largura do modal.
+		 */
+		disabled: {
+			type: Boolean,
+			default: false,
 		},
 	},
 
@@ -68,14 +108,13 @@ export default {
 
 	watch: {
 		show(value) {
-			this.internalShow = this.show;
+			this.internalShow = value;
 		},
 	},
 
 	computed: {
 		dynamicStyle() {
 			return {
-				'--right': `${this.right}px`,
 				'--top': `${this.top}px`,
 				'--height': `${this.height}px`,
 				'--width': `${this.width}px`,
@@ -84,15 +123,24 @@ export default {
 	},
 
 	methods: {
-		hide () {
+		close() {
 			/**
 			 * Evento que indica se o modal foi escondido.
-			* @event hide
+			* @event close
 			* @type {Event}
 			*/
-			this.$emit('hide', true);
+			this.$emit('close', true);
 			this.internalShow = !this.internalShow;
-		}
+		},
+		ok() {
+			/**
+			 * Evento que indica se o botão de ação do modal foi clicado.
+			* @event ok
+			* @type {Event}
+			*/
+			this.$emit('ok', true);
+			this.internalShow = !this.internalShow;
+		},
 	},
 };
 </script>
@@ -100,15 +148,47 @@ export default {
 @import '../assets/sass/app.scss';
 
 .cds-modal {
+	display: flex;
+	flex-direction: column;
+	min-height: 150px;
 	height: var(--height);
 	width: var(--width);
 	position: absolute;
 	background-color: white;
-	padding: 20px;
-	right: var(--right);
+	padding: 32px 28px;
+	right: calc(50% - var(--width) / 2);
 	top: var(--top);
 	border-radius: 8px;
 	box-shadow: 0px 0px 8px rgba(40, 90, 185, 0.2);
-	z-index:999999999;
+	overflow-x: auto;
+	z-index: 999999999;
+
+	&__backdrop {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		position: fixed;
+		top: 0;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		background-color: rgba(0, 0, 0, 0.45);
+		z-index: 99999999;
+	}
+
+	&__header {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	&__close-icon {
+		cursor: pointer;
+	}
+
+	&__footer {
+		display: flex;
+		justify-content: end;
+		margin-top: auto;
+	}
 }
 </style>
