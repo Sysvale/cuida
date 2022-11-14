ff54de170bb2fd907f046c9f4423d19ab22db951
Hosana <hosanabarceloscandido@gmail.com>
2022-11-14 08:46:50 -0300

    feat: importa componente textarea

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -68,6 +68,7 @@ import {
 	TextInput,
 	Pagination,
 	DateInput,
+    TextArea,
 } from '../src/components';
 
 import {
@@ -163,6 +164,7 @@ Vue.component('cds-carousel-controller', CarouselController);
 Vue.component('cds-text-input', TextInput);
 Vue.component('cds-pagination', Pagination);
 Vue.component('cds-date-input', DateInput);
+Vue.component('cds-text-area', TextArea);
 
 addParameters({
 	options: {
