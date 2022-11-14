cc6e216b02dd72880f74e6b2e40fc81b79c8f767
Hosana <hosanabarceloscandido@gmail.com>
2022-11-14 12:12:17 -0300

    fix: corrige alinhamento de radio

--- src/components/Radio.vue
+++ src/components/Radio.vue
@@ -13,6 +13,7 @@
 			:name="$attrs.name"
 			:checked="isChecked"
 			@change="$emit('change', $event.target.value)"
+            class="cds-radio__input"
 		>
 		<label
 			class="cds-radio__label"
@@ -81,99 +82,60 @@ export default {
 <style lang="scss">
 @import '../assets/sass/app.scss';
 
-.cds-radio__label {
-	@include body-2;
-
-	[disabled="disabled"] {
-		color: $n-300;
-	}
+*:before,
+*:after {
+  box-sizing: border-box;
 }
 
 .cds-radio {
-	[type="radio"] {
-		&:checked,
-		&:not(:checked) {
-			position: absolute;
-			left: -9999px;
-		}
+    display: flex;
+    align-items: flex-start;
+}
 
-		&:checked + label,
-		&:not(:checked) + label {
-			position: relative;
-			padding: pl(7);
-			cursor: pointer;
-			line-height: 16px;
-			display: inline-block;
-			color: $n-700;
+.cds-radio__label {
+    @include body-2;    
+    margin-left: 5px;
 
-			&:before {
-				content: '';
-				position: absolute;
-				left: 0;
-				top: 0;
-				width: 16px;
-				height: 16px;
-				border: 1px solid $n-400;
-				border-radius: 100%;
-				background: $n-0;
-			}
+    [disabled="disabled"] {
+        color: $n-300;
+    }
+}
 
-			&:after {
-				content: '';
-				width: 8px;
-				height: 8px;
-				background: $gp-500;
-				border: 1px solid $gp-500;
-				position: absolute;
-				top: 3.7px;
-				left: 3.7px;
-				border-radius: 100%;
-				-webkit-transition: all 0.3s ease;
-				transition: all 0.3s ease;
-			}
-		}
+input[type="radio"] {
+    -webkit-appearance: none;
+    appearance: none;
+    margin: 0;
+    font: inherit;
+    width: 16px;
+    height: 16px;
+    border: 1px solid $gp-500;
+    border-radius: 50%;
+    display: grid;
+    place-content: center;
+}
 
-		&:checked + label {
-			&:before {
-				border: 1px solid $gp-500;
-			}
+input[type="radio"]::before {
+    content: "";
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    transform: scale(0);
+    transition: 120ms transform ease-in-out;
+    background: $gp-500;
+}
 
-			&:after {
-				opacity: 1;
-				-webkit-transform: scale(1);
-				transform: scale(1);
-			}
-		}
+input[type="radio"]:checked::before {
+  transform: scale(1);
+}
 
-		&:not(:checked) + label {
-			&:after {
-				opacity: 0;
-				-webkit-transform: scale(0);
-				transform: scale(0);
-			}
-		}
-	}
+input[type="radio"][disabled="disabled"] {
+    -webkit-transition: all 0.3s ease;
+    transition: all 0.3s ease;
+    background: $n-20;
+    border: 1px solid $n-50;
+}
 
-	input[type="radio"][disabled="disabled"]:not(:checked) + label:before {
-		-webkit-transition: all 0.3s ease;
-		transition: all 0.3s ease;
-		background: $n-20;
-		border: 1px solid $n-50;
-	}
-
-	input[type="radio"][disabled="disabled"]:checked + label:before {
-		-webkit-transition: all 0.3s ease;
-		transition: all 0.3s ease;
-		border: 1px solid $n-50;
-		color: $n-50;
-	}
-
-	input[type="radio"][disabled="disabled"]:checked + label:after {
-		-webkit-transition: all 0.3s ease;
-		transition: all 0.3s ease;
-		border: 1px solid $n-50;
-		background: $n-50;
-		color: $n-50;
-	}
+input[type="radio"][disabled="disabled"]::before {
+    background: $n-20;
 }
 </style>
