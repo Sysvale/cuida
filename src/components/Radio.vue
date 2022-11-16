04f71283af639b784f295048270320f279536cd9
Patrícia Coelho <patriciacoelho.slv@gmail.com>
2022-11-16 08:39:05 -0300

    fix: ajusta identação

--- src/components/Radio.vue
+++ src/components/Radio.vue
@@ -6,14 +6,14 @@
 			@type {Event}
 		-->
 		<input
-			type="radio"
 			:id="value"
+			class="cds-radio__input"
+			type="radio"
 			:value="value"
 			:disabled="disabled"
 			:name="$attrs.name"
 			:checked="isChecked"
 			@change="$emit('change', $event.target.value)"
-            class="cds-radio__input"
 		>
 		<label
 			class="cds-radio__label"
@@ -84,59 +84,59 @@ export default {
 
 *:before,
 *:after {
-  box-sizing: border-box;
+	box-sizing: border-box;
 }
 
 .cds-radio {
-    display: flex;
-    align-items: flex-start;
+	display: flex;
+	align-items: flex-start;
 }
 
 .cds-radio__label {
-    @include body-2;    
-    margin-left: 10px;
+	@include body-2;    
+	margin-left: 10px;
 
-    [disabled="disabled"] {
-        color: $n-300;
-    }
+	[disabled="disabled"] {
+		color: $n-300;
+	}
 }
 
 input[type="radio"] {
-    -webkit-appearance: none;
-    appearance: none;
-    margin: 0;
-    font: inherit;
-    width: 16px;
-    height: 16px;
-    border: 1px solid $n-50;
-    border-radius: 50%;
-    margin-top: 1.5px;
-    display: grid;
-    place-content: center;
+	-webkit-appearance: none;
+	appearance: none;
+	margin: 0;
+	font: inherit;
+	width: 16px;
+	height: 16px;
+	border: 1px solid $n-50;
+	border-radius: 50%;
+	margin-top: 1.5px;
+	display: grid;
+	place-content: center;
 }
 
 input[type="radio"]:checked {
-    border: 1px solid $gp-500;
+	border: 1px solid $gp-500;
 }
 
 input[type="radio"]::before {
-    content: "";
-    width: 8px;
-    height: 8px;
-    border-radius: 50%;
-    transform: scale(0);
-    transition: 120ms transform ease-in-out;
-    background: $gp-500;
+	content: "";
+	width: 8px;
+	height: 8px;
+	border-radius: 50%;
+	transform: scale(0);
+	transition: 120ms transform ease-in-out;
+	background: $gp-500;
 }
 
 input[type="radio"]:checked::before {
-    transform: scale(1);
+	transform: scale(1);
 }
 input[type="radio"][disabled="disabled"] {
-    border: 1px solid $n-50;
-    background: $n-20;
+	border: 1px solid $n-50;
+	background: $n-20;
 }
 input[type="radio"][disabled="disabled"]::before {
-    background: $n-20;
+	background: $n-20;
 }
 </style>
