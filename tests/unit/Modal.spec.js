c30c02ba7b48bd90034bdf980920c20073166035
Patrícia Coelho <patriciacoelho.slv@gmail.com>
2022-11-24 13:07:44 -0300

    feat: adiciona alguns casos de teste do Modal

--- tests/unit/Modal.spec.js
+++ tests/unit/Modal.spec.js
@@ -1,12 +1,13 @@
 import { mount, shallowMount, createLocalVue } from '@vue/test-utils';
 import Modal from '../../src/components/Modal.vue';
 import { directive as onClickOutside } from 'vue-on-click-outside';
+import flushPromises from 'flush-promises';
 
 const localVue = createLocalVue();
 
 localVue.directive('on-click-outside', onClickOutside);
 
-test('Component is mounted properly', () => {
+test('Component is mounted properly', async () => {
 	const wrapper = shallowMount(Modal, {
 		localVue,
 		slots: {
@@ -14,12 +15,90 @@ test('Component is mounted properly', () => {
 		},
 		propsData: {
 			show: true,
-			top: 20,
-			right: 20,
-			height: 293,
-			width: 313,
+		},
+		stubs: {
+			'cds-button': true,
+			'cds-scrollable': true,
+			'ion-icon': true,
 		},
 	});
 
+	await flushPromises();
+
 	expect(wrapper).toMatchSnapshot();
+});
+
+describe('default content, classes and attributes',  () => {
+	test('modal is hidden when show is false', async () => {
+		const wrapper = mount(Modal, {
+			localVue,
+			slots: {
+				default: '<div>modal content</div>',
+			},
+			propsData: {
+				show: false,
+			},
+			stubs: {
+				'cds-button': true,
+				'cds-scrollable': true,
+				'ion-icon': true,
+			},
+		});
+
+		await flushPromises();
+
+		expect(wrapper.find('.cds-modal').exists()).toBe(false);
+	});
+
+	test('structure is as expected when title, size, okButtonText e cancelButtonText is defined', async () => {
+		const wrapper = mount(Modal, {
+			localVue,
+			slots: {
+				default: '<div>modal content</div>',
+			},
+			propsData: {
+				show: true,
+				title: 'Dummy title',
+				okButtonText: 'Confirmar',
+				cancelButtonText: 'Cancelar',
+				size: 'sm',
+			},
+			stubs: {
+				'cds-button': true,
+				'cds-scrollable': true,
+				'ion-icon': true,
+			},
+		});
+
+		await flushPromises();
+
+		// applies size class
+		expect(wrapper.find('.cds-modal--sm').exists()).toBe(true);
+
+		// applies title prop
+		expect(wrapper.find('.cds-modal__header').text()).toContain('Dummy title');
+
+		// applies okButtonText prop
+		expect(wrapper.find('.cds-modal__footer').html()).toContain('text="Confirmar"');
+
+		// applies cancelButtonText prop
+		expect(wrapper.find('.cds-modal__footer').html()).toContain('text="Cancelar"');
+	});
+});
+
+describe('custom content, attributes and slots', () => {
+	// if close modal is unavailable when noCloseOnBackdrop, noCloseButton e noCancelButton is set
+	
+	// if dont show default footer when noFooter is true
+	// if dont show default header when noHeader is true
+	
+	// if applies a custom header slot as expected 
+	// if applies a custom footer slot as expected 
+});
+
+describe('button and event functionality', () => {
+	// if header close button triggers modal close
+	// if footer cancel button triggers modal close
+	// if footer ok button triggers modal ok
+	// if click outside closes modal
 });
