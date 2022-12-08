c008886ff320e48b559cfab0f1af9b5637d60e9a
RafaelGondi <rdias.ga@gmail.com>
2022-12-08 16:58:59 -0300

    feat: Cria util de diretiva cdstippy

---
+++ src/utils/directives/cdstip.js
@@ -0,0 +1,25 @@
+import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css';
+
+export default (el, binding) => {
+	const generateRandomId = () => {
+		const s4 = () => {
+			return Math.floor((1 + Math.random()) * 0x10000)
+				.toString(16)
+				.substring(1);
+		};
+
+		return `${s4()}-${s4()}`;
+	};
+
+	if (binding.value === null || binding.value === undefined) return;
+
+	const tippyId = generateRandomId();
+	el.setAttribute('data-tippy', tippyId);
+
+	const elementQueryString = `${el.tagName.toLowerCase()}[data-tippy="${tippyId}"]`;
+
+	tippy(elementQueryString, {
+		content: binding.value,
+	});
+};
