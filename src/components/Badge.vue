0e55f09ab12896388e94ea4eb28896b8bcfecee0
RafaelGondi <rdias.ga@gmail.com>
2023-01-05 10:42:51 -0300

    feat: Implementa prop de tamanho na Badge

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -3,17 +3,21 @@
 		class="badge__container"
 		:class="predefinedStyle"
 	>
-		<div class="badge__content">
+		<div
+			:class="{
+				'badge__content--sm': size === 'sm',
+				'badge__content--md': size === 'md',
+				'badge__content--lg': size === 'lg',
+			}"
+		>
 			<!-- @slot Slot padrão utilizado para exibir o conteúdo dentro da badge -->
 			<slot/>
 		</div>
 	</div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
+<script>
+export default {
 	props: {
 		/**
 		 * A variante da Badge. São 9 variantes: 'turquoise', 'green', 'blue',
@@ -22,6 +26,13 @@ export default defineComponent({
 		variant: {
 			type: String,
 			default: 'gray',
+		},
+		/**
+		 * Especifica o tamanho da badge. São 3 tamanhos implementados: 'sm', 'md', 'lg'.
+		 */
+		size: {
+			type: String,
+			default: 'md',
 		},
 	},
 
@@ -38,19 +49,32 @@ export default defineComponent({
 				'orange',
 				'amber',
 				'gray',
+				'white',
+			],
+			predefinedSizes: [
+				'sm',
+				'md',
+				'lg',
 			],
 		};
 	},
 
 	computed: {
 		predefinedStyle() {
+			let dynamicClass = '';
+
 			if (this.predefinedColors.indexOf(this.variant) > -1) {
-				return `badge--${this.variant}`;
+				dynamicClass += `badge--${this.variant}`;
 			}
-			return 'badge--gray';
+
+			if (this.predefinedSizes.indexOf(this.size) > -1) {
+				dynamicClass += ` badge--${this.size}`;
+			}
+
+			return dynamicClass;
 		},
 	},
-});
+};
 </script>
 <style lang="scss" scoped>
 @import '../assets/sass/tokens.scss';
@@ -59,12 +83,34 @@ export default defineComponent({
 	&__container {
 		border-radius: 50px !important;
 		width: fit-content;
+	}
+
+	&--sm {
+		padding: pYX(1, 2);
+	}
+
+	&--md {
+		padding: pYX(1, 2);
+	}
+
+	&--lg {
 		padding: pYX(1, 3);
 	}
 
 	&__content {
-		@include body-1;
-		font-weight: $font-weight-medium;
+		&--sm {
+			@include overline;
+		}
+
+		&--md {
+			@include caption;
+			font-weight: $font-weight-medium;
+		}
+
+		&--lg {
+			@include button-2;
+			font-weight: $font-weight-medium;
+		}
 	}
 
 	&--turquoise {
@@ -115,6 +161,12 @@ export default defineComponent({
 	&--gray {
 		color: $n-600;
 		background-color: $n-20;
+	}
+
+	&--white {
+		color: $n-600;
+		background-color: $n-0;
+		outline: 1px solid $n-50;
 	}
 }
 </style>
