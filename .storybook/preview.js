bfa263485b5cc710f5e192e41567231fbd75d9c5
RafaelGondi <rdias.ga@gmail.com>
2023-01-13 11:06:34 -0300

    build: Configura o preview.js como entrypoint vue

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,48 +1,53 @@
 //NOTE: nem todos os parâmetros estão funcionando aqui
-// import Vue from 'vue';
-
-// import {
-// 	Badge,
-// } from '../src/components';
-
-// Vue.component('cds-badge', Badge);
-
-// export const parameters = {
-//   actions: { argTypesRegex: "^on[A-Z].*" },
-//   viewMode: "docs",
-//   options: {
-//     previewTabs: {
-//       canvas: { hidden: true },
-//     },
-//     storySort: {
-//       order: [
-//         "Fundação",
-//         [
-//           "Princípios",
-//           "Tipografia",
-//           "Peso da Fonte",
-//           "Cores",
-//           "Espaçamento",
-//           "Bordas",
-//         ],
-//         "Componentes",
-//       ],
-//     },
-//   },
-//   controls: {
-//     matchers: {
-//       color: /(background|color)$/i,
-//       date: /Date$/,
-//     },
-//   },
-// };
-
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+	actions: { argTypesRegex: "^on[A-Z].*" },
+	viewMode: "docs",
+	options: {
+		previewTabs: {
+			canvas: { hidden: true },
+		},
+		storySort: {
+			order: [
+				"Fundação",
+				[
+					"Princípios",
+					"Cores",
+					"Tipografia",
+					"Peso da Fonte",
+					"Espaçamento",
+					"Bordas",
+				],
+				"Componentes",
+				[
+					"Display",
+					"Notificação",
+					"Forms",
+					"Navegação",
+					"Containers",
+					"Overlays",
+					"Loaders",
+					"Estrutural",
+					"Utils",
+				],
+			],
+		},
+	},
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
 };
+
+import { app } from "@storybook/vue3";
+import { longClickDirective } from '@sysvale/vue3-long-click';
+// import tippy from 'tippy.js';
+// import 'tippy.js/dist/tippy.css';
+import Cdstip from '../src/utils/directives/cdstip';
+
+const longClickInstance = longClickDirective({delay: 400, interval: 50});
+
+app.directive('longclick', longClickInstance);
+app.directive('cdstip', Cdstip);
+app.mount('#app');
