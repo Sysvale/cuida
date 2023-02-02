64e539bfc68025cf2a2cd7eaedcf129b06784e5c
RafaelGondi <rdias.ga@gmail.com>
2023-02-02 16:29:26 -0300

    test: Cria teste de snapshot do Box

---
+++ src/tests/Box.spec.js
@@ -0,0 +1,21 @@
+// @vitest-environment jsdom
+import { describe, test, expect } from 'vitest';
+import Box from '../components/Box.vue';
+import { mount } from '@vue/test-utils';
+
+describe('Spacer snapshot test', () => {
+	test('renders correctly', () => {
+		const wrapper = mount(Box, {
+			props: {
+				elevated: false,
+				fluid: false,
+				padding: 4,
+			},
+			slots: {
+				default: '<div style="width: 100px; height: 100px; background-color: #2AC092; border-radius: 20px"/>'
+			}
+		});
+
+		expect(wrapper).toMatchSnapshot();
+	});
+});
