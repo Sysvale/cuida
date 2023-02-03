376e5627632c0118f31eb88014cdde5fa23795d4
RafaelGondi <rdias.ga@gmail.com>
2023-02-03 11:15:27 -0300

    feat: Cria diretiva floatify

---
+++ src/utils/directives/cdsFloatify.js
@@ -0,0 +1,28 @@
+import { createPopper } from '@popperjs/core';
+
+export default (el, biding) => {
+	let target = '';
+	let popoverElement = '';
+	let modifiers = biding.modifiers;
+	let position = biding.arg;
+
+	target = document.querySelector(`[id='${biding.value}']`);
+	popoverElement = document.querySelector(`[id='${el.id}']`);
+	
+	createPopper(target, popoverElement, {
+		placement: position,
+		modifiers: [
+			{
+				name: 'offset',
+				options: {
+					offset: [0, -4],
+				},
+			},
+			{
+				name: 'flip',
+				enabled: !!modifiers.flip,
+			},
+		],
+	});
+
+};
