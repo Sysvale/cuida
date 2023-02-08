b0ca45a4661ae2827600fec624c25ec616139d63
RafaelGondi <rdias.ga@gmail.com>
2023-02-08 13:08:39 -0300

    refactor: Reestrutura o timelineItem

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -1,51 +1,51 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-	<span class="cds-timeline">
-		<div
-			class="container"
-		>
-			<div>
-				<div class="event__row">
-					<div class="event__date">
-						<slot name="opposite" />
-					</div>
-
-					<div
-						class="timeline__container"
-					>
-						<span
-							v-if="!loading"
-							:class="`event__pin--${variant}`"
-						/>
-						<cds-spinner
-							v-else
-							size="sm"
-							class="spinner"
-							:variant="variant"
-						/>
-
-						<div
-							:class="(loading || hollowed) ? 'timeline--dashed' : 'timeline'"
-						/>
-					</div>
-
-					<div class="content__container">
-						<div class="content__title">
-							<slot name="title" />
-						</div>
-
-						<span class="content__text">
-							<slot name="content" />
-						</span>
-					</div>
-				</div>
-			</div>
+	<div class="cds-timeline-item">
+		<div class="cds-timeline-item__opposite">
+			<!-- @slot Slot utilizado para renderização de conteúdo no lado esquerdo do TimelineItem.-->
+			<slot name="opposite" />
 		</div>
-	</span>
+
+		<div
+			class="cds-timeline-item__timeline-container"
+		>
+			<span
+				v-if="!loading"
+				:class="`cds-timeline-item__pin--${variant}${hollowed ? '--hollowed' : ''}`"
+			/>
+
+			<cds-spinner
+				v-else
+				size="sm"
+				class="spinner"
+				:variant="variant"
+			/>
+
+			<div
+				:class="(loading || hollowed) ? 'tail--dashed' : 'tail'"
+			/>
+		</div>
+
+		<div class="cds-timeline-item__content-container">
+			<div
+				v-if="hasSlot($slots, 'title')"
+				class="cds-timeline-item__title"
+			>
+				<!-- @slot Slot utilizado para renderização do título do TimelineItem.-->
+				<slot name="title" />
+			</div>
+
+			<span class="cds-timeline-item__text">
+				<!-- @slot Slot utilizado para renderização do conteúdo do TimelineItem. Os elementos adicionados nesse slot ficam dispostos abaixo do título. -->
+				<slot name="content" />
+			</span>
+		</div>
+	</div>
 </template>
 
 <script>
 import CdsSpinner from './Spinner.vue';
+import hasSlot from '../utils/methods/hasSlot';
 
 export default {
 	components: {
@@ -54,137 +54,96 @@ export default {
 
 	props: {
 		/**
-		* O array que especifica os eventos que vão ser mostrados na timeline
-		*/
-		reverse: {
-			type: Boolean,
-			default: false,
-		},
-		/**
-		* O array que especifica os eventos que vão ser mostrados na timeline
+		* A variante do TimelineItem. São 10 variantes: 'teal', 'green', 'blue',
+		* 'violet', 'pink', 'red', 'orange', 'amber', 'gray' e 'dark'.
 		*/
 		variant: {
 			type: String,
 			default: 'green',
 		},
 		/**
-		* O array que especifica os eventos que vão ser mostrados na timeline
+		* Altera o estado do pin. Quando true torna o pin em um `Cds-spinner`.
 		*/
 		loading: {
 			type: Boolean,
 			default: false,
 		},
 		/**
-		* O array que especifica os eventos que vão ser mostrados na timeline
+		* Altera o estado do pin. Quando true deixa o pin sem preenchimento, apenas com borda.
 		*/
 		hollowed: {
 			type: Boolean,
 			default: false,
 		},
-		opposite: {
-			type: String,
-			default: null,
-		},
-		title: {
-			type: String,
-			default: null,
-		},
-		content: {
-			type: String,
-			default: null,
-		},
 	},
 
-	data() {
-		return {
-			innerHistory: this.reverse ? this.history.slice().reverse() : this.history,
-		};
+	methods: {
+		hasSlot,
 	},
 };
 </script>
 <style lang="scss" scoped>
 @import '../assets/sass/tokens.scss';
 
-.cds-timeline {
-	.container {
-		// padding: pa(5) !important;
-	}
-	
-	.content__container {
-		// width: 50% !important;
-		margin-top: spacer(n1);
-		margin-left: spacer(6);
-		margin-bottom: spacer(10);
-		
-		& > .divider__container {
-			margin: my(3);
-		}
+.cds-timeline-item {
+	display: flex;
+
+	&__opposite {
+		color: $n-600;
+		margin: mTRBL(n1, 6, 0, 0);
+		@include body-2;
 	}
 
-	.content__title {
+	&__content-container {
+		margin: mTRBL(n1, 0 , 8, 6);
+
+	}
+
+	&__title {
 		color: $n-900;
 		font-weight: 600;
 		margin: mb(2);
 	}
 	
-	.content__text {
+	&__text {
 		@include caption;
+		color: $n-600;
 		font-weight: $font-weight-regular;
-		color: $n-600;
-		margin: mt(1);
-		margin: mb(2);
 	}
 	
-	.event__date {
-		color: $n-600;
-		@include body-2;
-		margin: mTRBL(n1, 6, 0, 0);
-	}
-	
-	.event__pin {
+	&__pin {
+		border-radius: $border-radius-circle;
 		min-height: 16px !important;
 		min-width: 16px !important;
-		border-radius: $border-radius-circle;
 
 		@include variantResolver using ($color-name, $base-color, $disabled, $muted, $hover) {
 			background-color: $base-color;
-			@extend .event__pin;
+			@extend .cds-timeline-item__pin;
+
+			&--hollowed {
+				border: 4px solid $base-color;
+				box-sizing: border-box;	
+				@extend .cds-timeline-item__pin;
+			}
 		}
-	
-		&--filled {
-			@extend .event__pin;
-			background-color: $gp-500;
-		}
-	
-		&--hollowed {
-			@extend .event__pin;
-			border: 4px solid $gp-500;
-			box-sizing: border-box;
-		}
-	}
-	
-	.event__row {
-		display: flex;
-		// justify-content: space-between;
-	}
-	
-	.timeline {
-		height: 100% !important;
-		width: 2px !important;
-		background-color: $n-40;
 	}
 
-	.timeline--dashed {
-		height: 100% !important;
-		width: 2px !important;
-		border: 1px dashed $n-40;
-	}
-	
-	.timeline__container {
-		// background-color: rgb(231, 143, 143);
+	&__timeline-container {
+		align-items: center;
 		display: flex;
 		flex-direction: column;
-		align-items: center;
+	}
+	
+	.tail {
+		background-color: $n-40;
+		height: 100% !important;
+		width: 2px !important;
+	}
+
+	.tail--dashed {
+		border: 1px dashed $n-40;
+		height: 100% !important;
+		width: 2px !important;
 	}
 }
 
