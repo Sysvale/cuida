b938a5f033c53836204daa1073a5f98bf24eb65e
RafaelGondi <rdias.ga@gmail.com>
2023-02-12 21:41:56 -0300

    feat: Cria função rounder

---
+++ src/utils/methods/rounder.js
@@ -0,0 +1,7 @@
+export default function rounder(elementWidth, limiter = 24) {
+	let rawRadii = elementWidth / 8;
+	let radiiMod4 = rawRadii % 4;
+	let borderRadius = rawRadii > limiter ? limiter : (rawRadii - radiiMod4) + 4;
+
+	return `${borderRadius}px`;
+}
