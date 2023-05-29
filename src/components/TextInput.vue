b8f83f13198252088df7696f6583e0024b43424a
Edwildson C. Rodrigues <edwildson@hotmail.com>
2023-05-29 14:17:59 -0300

    feat: adiciona debouncer ao text input

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -220,12 +220,20 @@ export default {
 			type: String,
 			default: 'https://cuida.framer.wiki/',
 		},
+		/**
+		* Define a utilização de lazy para debouncer.
+		*/
+		lazy: {
+			type: Boolean,
+			default: false,
+		},
 	},
 
 	data() {
 		return {
 			internalValue: '',
 			isBeingFocused: false,
+			timeout: null,
 		};
 	},
 
@@ -304,9 +312,24 @@ export default {
 			 * @event input
 			 * @type {Event}
 			 */
-			this.$emit('update:modelValue', value);
+
+			if (this.lazy) {
+				this.emitLazy(value)
+			} else this.$emit('update:modelValue', value);
 		},
 	},
+
+	methods: {
+		emitLazy(value) {
+			clearTimeout(this.timeout);
+
+			const that = this;
+
+			this.timeout = setTimeout(function () {
+				that.$emit('update:modelValue', value);
+			}, 1000);
+		}
+	}
 };
 </script>
 <style lang="scss" scoped>
