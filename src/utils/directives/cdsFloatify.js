ba1855419b46c15b2409eca33252c0bb3236352f
RafaelGondi <rdias.ga@gmail.com>
2023-06-09 11:21:51 -0300

    style: Ajusta identação dos arquivos de utils

--- src/utils/directives/cdsFloatify.js
+++ src/utils/directives/cdsFloatify.js
@@ -1,27 +1,27 @@
-import { createPopper } from "@popperjs/core";
+import { createPopper } from '@popperjs/core';
 
 export default (el, biding) => {
-  let target = "";
-  let popoverElement = "";
-  let modifiers = biding.modifiers;
-  let position = biding.arg;
+	let target = '';
+	let popoverElement = '';
+	let modifiers = biding.modifiers;
+	let position = biding.arg;
 
-  target = document.querySelector(`[id='${biding.value}']`);
-  popoverElement = document.querySelector(`[id='${el.id}']`);
+	target = document.querySelector(`[id='${biding.value}']`);
+	popoverElement = document.querySelector(`[id='${el.id}']`);
 
-  createPopper(target, popoverElement, {
-    placement: position,
-    modifiers: [
-      {
-        name: "offset",
-        options: {
-          offset: [0, -4],
-        },
-      },
-      {
-        name: "flip",
-        enabled: !!modifiers.flip,
-      },
-    ],
-  });
+	createPopper(target, popoverElement, {
+		placement: position,
+		modifiers: [
+			{
+				name: 'offset',
+				options: {
+					offset: [0, -4],
+				},
+			},
+			{
+				name: 'flip',
+				enabled: !!modifiers.flip,
+			},
+		],
+	});
 };
