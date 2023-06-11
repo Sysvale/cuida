fd63005c280c9fcc3f30d8a6f974a71b710e3336
Igor <igor.vinicius@discente.univasf.edu.br>
2023-06-11 11:58:36 -0300

    fix: Remove stubs do tests

--- src/tests/Checkbox.spec.ts
+++ src/tests/Checkbox.spec.ts
@@ -5,11 +5,6 @@ import { mount } from '@vue/test-utils';
 describe('Button', () => {
 	test('renders correctly', async () => {
 		const wrapper = mount(Button, {
-			global: {
-				stubs: {
-					'cds-tooltip': true,
-				},
-			},
 			props: {
 				value: false,
 				label: 'checkbox test'
