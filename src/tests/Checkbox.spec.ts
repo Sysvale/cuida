ce08f67d01ec6064fb1c839a9f568cb9aead2c20
Igor <igor.vinicius@discente.univasf.edu.br>
2023-06-11 11:42:36 -0300

    feat: Altera test

--- src/tests/Checkbox.spec.ts
+++ src/tests/Checkbox.spec.ts
@@ -5,9 +5,15 @@ import { mount } from '@vue/test-utils';
 describe('Button', () => {
 	test('renders correctly', async () => {
 		const wrapper = mount(Button, {
+			global: {
+				stubs: {
+					'cds-button': true,
+					'cds-tooltip': true,
+				},
+			},
 			props: {
-				value: false,
-				label: 'checkbox test'
+				disabled: true,
+				tooltipText: 'Tooltip test'
 			},
 		});
   
