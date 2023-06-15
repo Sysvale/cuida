1d991c525ae5e6882765a3513cb09866678ca5a1
RafaelGondi <rdias.ga@gmail.com>
2023-06-15 16:40:38 -0300

    style: Torna código mais claro no paleteBuilder

--- src/utils/methods/paleteBuilder.js
+++ src/utils/methods/paleteBuilder.js
@@ -1,8 +1,8 @@
 export default (rawPalete) => {
-	let arr = [];
-	let obj = {};
+	let palete = [];
+	let colorSwatch = {};
 
-	const regexp = /(\[[^\[\]]*\])/g;
+	const regexp = /(\[[^[\]]*\])/g;
 	const matches = rawPalete.match(regexp);
 
 	for (let i = 0; i < matches.length; i++){
@@ -11,21 +11,37 @@ export default (rawPalete) => {
 
 		let properties = matches[i].split(',');
 
-		obj.colorName = properties[0].trim().replace(/"/g,'');
-		obj.colorShades = [];
-		obj.colorTokens = [];
+		colorSwatch.colorName = properties[0].trim().replace(/"/g,'');
+		colorSwatch.colorShades = [];
+		colorSwatch.colorTokens = [];
+		colorSwatch.colorData = [];
 
-		for (let j = 1; j < properties.length; j++) {
-			if (properties[j].includes('#')) {
-				obj.colorShades.push(properties[j].trim());
-			} else if (properties[j].includes('$')) {
-				obj.colorTokens.push(properties[j].trim().replace(/"/g,''));
+		let enteredTokensSection = false;
+
+
+		//NOTE: O idx3 é usado para retornar às primeiras posições do array para colocar na mesma posição
+		// valores HEX e os tokens 
+		for (let idx2 = 1, idx3 = 1; idx2 < properties.length; idx2++, idx3++) {
+			if (properties[idx2].includes('#')) {
+				colorSwatch.colorShades.push(properties[idx2].trim());
+
+				colorSwatch.colorData[idx3 - 1] = {};
+				colorSwatch.colorData[idx3 - 1].shade = properties[idx2].trim();
+			//NOTE: Entra no if abaixo quando o array de cores termina e começa o array de tokens
+			} else if (properties[idx2].includes('$')) {
+				colorSwatch.colorTokens.push(properties[idx2].trim().replace(/"/g,''));
+				colorSwatch.colorData[idx3 - 1].token = properties[idx2].trim().replace(/"/g,'');
+			}
+
+			if (!enteredTokensSection && properties[idx2 + 1].includes('$')) {
+				idx3 = 0;
+				enteredTokensSection = true;
 			}
 		}
 
-		arr.push(obj);
-		obj = {};
+		palete.push(colorSwatch);
+		colorSwatch = {};
 	}
 
-	return arr;
+	return palete;
 }
