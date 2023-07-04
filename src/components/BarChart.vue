3c242efacc2ea5eb2d514934d01da73326fdbad4
IhGori <igor.vinicius@discente.univasf.edu.br>
2023-07-04 14:37:26 -0300

    feat: Adiciona método para buscar dentro do arquivo sass para atulizar hex

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -1,5 +1,5 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<template> 
+<template>
 	<span id="cds-bar-chart">
 		<div
 			v-if="localSelect"
@@ -28,6 +28,7 @@ import { Chart, registerables } from 'chart.js';
 import { Bar } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 import CdsMultiselect from './Multiselect.vue';
+import sassColorVariables from '../assets/sass/colors.module.scss';
 
 // Registrar o elemento "point" no registro (Torna-se necessário para Line e Pie que necessita de marcações de ponto)
 Chart.register(...registerables);
@@ -102,64 +103,65 @@ export default {
 	},
 	data() {
 		return {
+			sassColorVariables,
 			localChartData: {},
 			localLabels: [],
 			localVariant: '',
 			palletColors: [
 				{
 					name: 'green',
-					colorHex: ['#ABEDD9', '#6DDFBC', '#2AC092', '#239F78', '#1B795B', '#126349'],
-					colorToken: ['$gp-200','$gp-300','$gp-400','$gp-500','$gp-600','$gp-700'],
+					colorHex: ['ABEDD9', '6DDFBC', '2AC092', '239F78', '1B795B', '126349'],
+					colorToken: ['gp200','gp300','gp400','gp500','gp600','gp700'],
 				},
 				{
 					name: 'turquoise',
-					colorHex: ['#C2EDFF', '#7BD0F4', '#42AAD7', '#1F86B2', '#18698B', '#13526D'],
-					colorToken: ['$ts-200','$ts-300','$ts-400','$ts-500','$ts-600','$ts-700'],
+					colorHex: ['C2EDFF', '7BD0F4', '42AAD7', '1F86B2', '18698B', '13526D'],
+					colorToken: ['ts200','ts300','ts400','ts500','ts600','ts700'],
 				},
 				{
 					name: 'blue',
-					colorHex: ['#B6D1F7', '#83ADE7', '#4B88DD', '#2C70CD', '#1A55A8', '#174382'],
-					colorToken: ['$bn-200', '$bn-300', '$bn-400', '$bn-500', '$bn-600', '$bn-700']
+					colorHex: ['B6D1F7', '83ADE7', '4B88DD', '2C70CD', '1A55A8', '174382'],
+					colorToken: ['bn200', 'bn300', 'bn400', 'bn500', 'bn600', 'bn700']
 				},
 				{
 					name: 'indigo',
-					colorHex: ['#CED6FD', '#AAB7F8', '#7080D2', '#4D5DAC', '#384584', '#353D64'],
-					colorToken: ['$in-200', '$in-300', '$in-400', '$in-500', '$in-600', '$in-700'],
+					colorHex: ['CED6FD', 'AAB7F8', '7080D2', '4D5DAC', '384584', '353D64'],
+					colorToken: ['in200', 'in300', 'in400', 'in500', 'in600', 'in700'],
 				},
 				{
 					name: 'violet',
-					colorHex: ['#E6D2F9', '#CEABED', '#A975D7', '#8955B9', '#6B3A98', '#4F2673'],
-					colorToken: ['$vr-200', '$vr-300', '$vr-400', '$vr-500', '$vr-600', '$vr-700'],
+					colorHex: ['E6D2F9', 'CEABED', 'A975D7', '8955B9', '6B3A98', '4F2673'],
+					colorToken: ['vr200', 'vr300', 'vr400', 'vr500', 'vr600', 'vr700'],
 				},
 				{
 					name: 'pink',
-					colorHex: ['#F9C8E2', '#EF8FC0', '#DA629F', '#BE377C', '#A12663', '#701A48'],
-					colorToken: ['$pp-200', '$pp-300', '$pp-400', '$pp-500', '$pp-600', '$pp-700'],
+					colorHex: ['F9C8E2', 'EF8FC0', 'DA629F', 'BE377C', 'A12663', '701A48'],
+					colorToken: ['pp200', 'pp300', 'pp400', 'pp500', 'pp600', 'pp700'],
 				},
 				{
 					name: 'red',
-					colorHex: ['#FABDC5', '#F98B98', '#F3596C', '#E03E52', '#C92C3F', '#A42333'],
-					colorToken: ['$rc-200', '$rc-300', '$rc-400', '$rc-500', '$rc-600', '$rc-700'],
+					colorHex: ['FABDC5', 'F98B98', 'F3596C', 'E03E52', 'C92C3F', 'A42333'],
+					colorToken: ['rc200', 'rc300', 'rc400', 'rc500', 'rc600', 'rc700'],
 				},
 				{
 					name: 'orange',
-					colorHex: ['#FFD6CC', '#FDAF9B', '#FF8567', '#F06442', '#D64B29', '#AB3C21'],
-					colorToken: ['$og-200', '$og-300', '$og-400', '$og-500', '$og-600', '$og-700'],
+					colorHex: ['FFD6CC', 'FDAF9B', 'FF8567', 'F06442', 'D64B29', 'AB3C21'],
+					colorToken: ['og200', 'og300', 'og400', 'og500', 'og600', 'og700'],
 				},
 				{
 					name: 'amber',
-					colorHex: ['#FFE2B8', '#FDCD87', '#FFB952', '#EEA22F', '#D38817', '#A56A12'],
-					colorToken: ['$al-200', '$al-300', '$al-400', '$al-500', '$al-600', '$al-700'],
+					colorHex: ['FFE2B8', 'FDCD87', 'FFB952', 'EEA22F', 'D38817', 'A56A12'],
+					colorToken: ['al200', 'al300', 'al400', 'al500', 'al600', 'al700'],
 				},
 				{
 					name: 'mid',
-					colorHex: ['#d6dce3', '#c3ccd5', '#acb8c3', '#99a6b2', '#8794a1'],
-					colorToken: ['$n-50', '$n-100', '$n-200',  '$n-300', '$n-400'],
+					colorHex: ['d6dce3', 'c3ccd5', 'acb8c3', '99a6b2', '8794a1'],
+					colorToken: ['n50', 'n100', 'n200',  'n300', 'n400'],
 				},
 				{
 					name: 'dark',
-					colorHex: ['#647382', '#52616f', '#3b4754', '#28333e', '#1d262f'],
-					colorToken: ['$n-500', '$n-600', '$n-700',  '$n-800', '$n-900'],
+					colorHex: ['647382', '52616f', '3b4754', '28333e', '1d262f'],
+					colorToken: ['n500', 'n600', 'n700',  'n800', 'n900'],
 				}
 			],
 			localSelect: '',
@@ -172,7 +174,7 @@ export default {
 			selectedValues: [],
 			chartOptions: {
 				responsive: true,
-				maintainAspectRatio: true, //NOTE: Caso true manterá aspecto de proporção original, caso false, será dimensionado para preencher completamente o contêiner (Isso pode fazer com que o gráfico pareça distorcido se o cntêiner tiver proporção de aspecto diferente do gráfico original)
+				maintainAspectRatio: true, //NOTE: Caso true manterá aspecto de proporção original, caso false, será dimensionado para preencher completamente o contêiner (Isso pode fazer com que o gráfico pareça distorcido se o container tiver proporção de aspecto diferente do gráfico original)
 				indexAxis: this.indexAxis, //NOTE: Configura o eixo do gráfico.
 				categoryPercentage: this.categoryPercentage, //NOTE: Configura a porcentagem ocupada pela barra do gráfico. (0-1)
 				plugins: {
@@ -199,8 +201,7 @@ export default {
 			return this.localSelect ? this.localChartData : this.verify;
 		},
 
-		// NOTE: Como modo de segurança, caso utilize um array de objetos irá exibir o primeiro objeto, caso não seja irá retornar somente o objeto passado
-		// eslint-disable-next-line vue/return-in-computed-property
+		// NOTE: Como modo de segurança, caso utilize um array de objetos irá exibir o primeiro objeto, caso não seja irá retornar somente o objeto passado	
 		verify() {
 			if (Array.isArray(this.chartData)) {
 				// eslint-disable-next-line vue/no-side-effects-in-computed-properties
@@ -268,6 +269,7 @@ export default {
 
 	mounted() {
 		this.addDataSetNames();
+		this.replaceColorsFromSass();
 	},
 
 	created() {
@@ -284,6 +286,20 @@ export default {
 	},
 
 	methods: {
+		// NOTE: Método que itera sobre palletColors e atualiza hex em acordo com o sassColorVariables
+		replaceColorsFromSass() {
+			this.palletColors.forEach((color) => {
+				color.colorToken.forEach((token, i) => {
+					const hex = this.sassColorVariables[token];
+
+					if (hex) {
+						const formattedHex = `${hex}`;
+						color.colorHex[i] = formattedHex;
+					}
+				});
+			});
+		},
+
 		// NOTE: Adiciona campo dataset.name com o nome do objeto respectivo
 		addDataSetNames() {
 			this.chartData.forEach(item => {
@@ -359,7 +375,6 @@ export default {
 			}
 			return backgroundColor;
 		},
-
   
 		// NOTE: Função responsável por setar backgroundColor, bordeWidth e name
 		// Ocorre essa verificação para garantir que o mesmo conjunto de dados para mais de um item selecionado tenha a mesma cor
