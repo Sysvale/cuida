fe4a0c8a616808cd8942de6a60c5716a307dc688
IhGori <igor.vinicius@discente.univasf.edu.br>
2023-08-27 12:48:54 -0300

    test: Adiciona testes do snapshot para Line, PolarArea e Pie

---
+++ src/tests/LineChart.spec.ts
@@ -0,0 +1,48 @@
+import { describe, test, expect } from 'vitest';
+import LineChart from '../components/LineChart.vue';
+import { shallowMount } from '@vue/test-utils';
+
+const mockedData = [
+	{
+		name: 'Ecocardiograma',
+		datasets: [
+			{
+				label: 'Realizado',
+				data: [50, 50, 50, 50, 50],
+			},
+		]
+	},
+	{
+		name: 'Raio-X',
+		datasets: [
+			{
+				label: 'Realizado',
+				data: [120, 220, 180, 320, 150],
+			},
+		]
+	},
+	{
+		name: 'Tomografia',
+		datasets: [
+			{
+				label: 'Realizado',
+				data: [55, 40, 33, 90, 120],
+			},
+		]
+	}
+];
+
+describe('LineChart', () => {
+	test('renders correctly', async () => {
+		const wrapper = shallowMount(LineChart, {
+			props: {
+				labels: ['Janeiro','Fevereiro','Março', 'Abril', 'Maio'],
+				variant: 'green',
+				showLabelName: true,
+				data: mockedData,
+			},
+		});
+
+		expect(wrapper).toMatchSnapshot();
+	});
+});
