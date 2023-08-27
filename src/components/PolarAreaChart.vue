823e1eed2d8772aca24f4833f85b7637036d28e6
IhGori <igor.vinicius@discente.univasf.edu.br>
2023-08-27 11:39:55 -0300

    feat: Cria componente PolarArea e o stories respectivo

---
+++ src/components/PolarAreaChart.vue
@@ -0,0 +1,233 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+	<span>
+		<div
+			class="responsive-container"
+		>
+			<PolarArea
+				:is="'polar-area'"
+				:data="localChartData"
+				:options="chartOptions"
+			/>
+		</div>
+	</span>
+</template>
+
+<script>
+import { PolarArea } from 'vue-chartjs'
+import sassColorVariables from '../assets/sass/colors.module.scss';
+import paleteBuilder from '../utils/methods/paleteBuilder.js';
+
+
+export default {
+	components: {
+		PolarArea,
+	},
+
+	props: {
+		/**
+		 * Define o conjunto de dados a serem mostrados no gráfico.
+		 * O objeto deve conter o parâmetro `name` (para identificar o conjunto de dados)
+		 * e `datasets`, array de objetos que apresentará `label` (indicar o rótulo do dado) e
+		 * `data` (array com os valores númericos).
+		 */
+		data: {
+			type: Object,
+			required: true,
+			default: () => ({
+				datasets: [
+					{
+						label: '',
+						data: [],
+					}
+				]
+			})
+		},
+		/**
+		 * Personaliza a paleta de cores do gráfico. São 11 variantes implementadas:
+		 * `green`,`turquoise`, `blue`, `indigo`, `violet`, `pink`, `red`, `orange`, `amber`, `mid`, `dark`.
+		 */
+		variant: {
+			type: String,
+			required: true,
+			default: 'green',
+			validator: (value) => {
+				return ['green', 'turquoise', 'blue', 'indigo', 'violet', 'pink', 'red', 'orange', 'amber', 'mid', 'dark'].includes(value);
+			}
+		},
+		/**
+		 * Defina as labels do gráfico
+		 */
+		labels: {
+			type: Array,
+			required: true,
+			default: () => [],
+		},
+		/**
+		 * Exibe o nome das labels centralizadas aos dados
+		 */
+		isVisiblePointNames: {
+			type: Boolean,
+			default: false,
+		},
+	},
+
+	data() {
+		return {
+			sassColorVariables,
+			localChartData: {},
+			localLabels: [],
+			palletColors: [],
+			deleteFirstTwoColors: false, //NOTE: Responsável por garantir que as cores mid e dark da paleta não serão removidos os dois primeiros elementos
+			chartOptions: {
+				responsive: true,
+				scales: {
+					r: {
+						pointLabels: {
+							display: this.isVisiblePointNames,
+							centerPointLabels: true,
+							font: {
+								size: 12
+							}
+						}
+					}
+				},
+				maintainAspectRatio: false, // NOTE: Caso true manterá aspecto de proporção original, caso false, será dimensionado para preencher completamente o contêiner (Isso pode fazer com que o gráfico pareça distorcido se o container tiver proporção de aspecto diferente do gráfico original)
+				pieceLabel: {
+					mode: 'percentage',
+					precision: 1
+				},
+				plugins: {
+					tooltip: {
+						callbacks: {
+							beforeTitle: function (context) {
+								return `${context[0].dataset.name}`;
+							},
+						}
+					},
+					datalabels: {
+						formatter: (value, context) => {
+							console.log(context);
+						}
+					}
+				}
+			},
+		}
+	},
+
+	watch: {
+		labels: {
+			handler(newValue) {
+				this.localLabels = newValue
+			},
+			immediate: true,
+		},
+
+		variant: {
+			handler(newValue) {
+				if (newValue === 'mid' || newValue === 'dark')  {
+					this.deleteFirstTwoColors = true;
+				} else {
+					this.deleteFirstTwoColors = false;
+				}
+			},
+			immediate: true,
+		},
+
+		data: {
+			handler(newValue) {
+				this.mergeChartDataNoSelect(newValue);
+			},
+			immediate: true,
+		},
+	},
+
+	methods: {
+		paleteBuilder,
+
+		palete() {
+			this.palletColors = this.paleteBuilder(this.sassColorVariables.palete);
+			this.removeFirstTwoElements();
+		},
+
+		// NOTE: Função responsável por remover os dois primeiros elementos da paleta para quando não é Mid ou Dark Neutrals
+		removeFirstTwoElements() {
+			for (let i = 0; i < this.palletColors.length; i++) {
+				const color = this.palletColors[i];
+
+				if (this.deleteFirstTwoColors === false) {
+					color.colorShades.splice(0, 2);
+					color.colorTokens.splice(0, 2);
+					color.colorData.splice(0, 2);
+				}
+			}
+		},
+
+		// NOTE: Adiciona campo dataset.name com o nome do objeto respectivo
+		addDataSetNames() {
+			this.data.forEach(item => {
+				item.datasets.forEach(dataset => {
+					dataset.name = item.name;
+				});
+			});
+		},
+
+		// NOTE: Função que recebe uma matriz de dados dos gráfico.
+		mergeChartDataNoSelect(data) {
+			// data.labels = this.localLabels;
+			const mergedData = { labels: this.localLabels, datasets: [] };
+			this.addDataSetNames();
+			data.forEach(obj => {
+				obj.datasets.forEach(state => {
+					const dataset = {
+						label: state.label,
+						data: state.data,
+						name: state.name,
+						borderRadius: 6,
+					};
+					mergedData.datasets.push(dataset);
+				});
+			});
+			this.palete();
+			const backgroundColor = this.generateBackgroundColor();
+			this.setColors(mergedData.datasets, backgroundColor);
+			this.localChartData = mergedData;
+		},
+
+		// NOTE: Função responsável por buscar a cor na paleta
+		generateBackgroundColor() {
+			const variantLowercase = this.variant.toLowerCase();
+			const palletColor = this.palletColors.find(color => color.variantName.toLowerCase().includes(variantLowercase));
+			if (palletColor) {
+				return palletColor.colorShades;
+			}
+			return [];
+		},
+
+		// NOTE: Função responsável por setar backgroundColor
+		// Ocorre essa verificação para garantir que o mesmo conjunto de dados para mais de um item selecionado tenha a mesma cor
+		setColors(datasets, backgroundColor) {
+			const colors = {};
+
+			datasets.forEach(dataset => {
+				const objectName = dataset.name;
+
+				if (!colors[objectName]) {
+					colors[objectName] = backgroundColor.slice();
+				}
+
+				dataset.backgroundColor = colors[objectName].splice(0, dataset.data.length);
+				dataset.borderRadius = 6;
+			});
+		},
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+@import './../assets/sass/tokens.scss';
+.responsive-container{
+	width: 100%;
+	height: 100%;
+}
+</style>
