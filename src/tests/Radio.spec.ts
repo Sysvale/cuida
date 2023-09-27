fd1ab8ab38e8e110a8abc5e817c745fa787b5ee4
Lucas Nascimento <lucas.barbosa.nas@gmail.com>
2023-09-27 15:23:25 -0300

    fix: Adiciona mock de função de geração de número aleatório

--- src/tests/Radio.spec.ts
+++ src/tests/Radio.spec.ts
@@ -1,9 +1,11 @@
-import { describe, test, expect } from 'vitest';
+import { describe, test, expect, vi } from 'vitest';
 import Radio from '../components/Radio.vue';
 import { mount } from '@vue/test-utils';
 
 describe('Radio', () => {
 	test('renders correctly', async () => {
+		Math.random = vi.fn(() => 0.1);
+
 		const wrapper = mount(Radio, {
 			props: {
 				modelValue: null,
