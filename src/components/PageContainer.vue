93b088a6e90bd441524612127a74fe5b5a309165
Rafael Gondi <rdias.ga@gmail.com>
2024-03-21 16:05:31 -0300

    feat: Cria componente PageContainer

---
+++ src/components/PageContainer.vue
@@ -0,0 +1,20 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+	<div class="page-container__container">
+		<slot />
+	</div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/sass/tokens.scss';
+
+.page-container__container {
+	width: 100%;
+	max-width: 1944px;
+	margin: 0 auto;
+	padding: pa(6);
+}
+</style>
