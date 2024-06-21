3a38bd762e3cf6774b9f5246c1e69caddf4f9dc6
Lucas Nascimento <lucas.barbosa.nas@gmail.com>
2024-06-21 13:39:44 -0300

    feat: Adiciona delay de exibição do spinner

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -1,6 +1,9 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-	<div :class="computedSpinnerClass" />
+	<div
+		v-if="showSpinner"
+		:class="computedSpinnerClass"
+	/>
 </template>
 
 <script>
@@ -23,6 +26,20 @@ export default {
 			type: String,
 			default: 'green',
 		},
+		/**
+		*	Delay para exibição do spinner, em ms
+		*
+		*/
+		delay: {
+			type: Number,
+			default: 0,
+		},
+	},
+
+	data() {
+		return {
+			showSpinner: false,
+		};
 	},
 
 	computed: {
@@ -37,6 +54,22 @@ export default {
 		computedSpinnerClass() {
 			return `${this.computedSizeClass} ${this.computedColorClass}`;
 		},
+	},
+
+	mounted() {
+		if (this.delay > 0) {
+			setTimeout(() => {
+				this.showSpinner = true;
+			}, this.delay);
+
+			return;
+		}
+
+		this.showSpinner = true;
+	},
+
+	unmounted() {
+		this.showSpinner = false;
 	},
 };
 </script>
