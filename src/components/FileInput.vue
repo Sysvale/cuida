1a58d4031d443a6c6e2bc3be1ed2cc4bc2a4dc66
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2024-06-27 09:44:15 -0300

    bugfix: corrige problema de nome do arquivo muito grande

--- src/components/FileInput.vue
+++ src/components/FileInput.vue
@@ -84,7 +84,7 @@
 						class="on-drag-content__container"
 						:class="{'on-drag-content__container--disabled': disabled === true}"
 					>
-						<div>{{ file.name }}</div>
+						<div>{{ formatFilename }}</div>
 						<div
 							class="x-icon__container"
 							@click.stop="handleRemove"
@@ -233,6 +233,20 @@ export default {
 
 		textAlignmentResolver() {
 			return this.size === 'sm' ? 'flex-start' : 'center';
+		},
+
+		formatFilename() {
+			if (this.file instanceof File && this.file.name.length > 16) {
+				const splitedName = this.file.name.split('.');
+
+				if (splitedName.length > 2) {
+					return this.file.name;
+				}
+
+				return `${splitedName[0].substring(0, 16)}....${splitedName[1]}`;
+			}
+
+			return this.file.name;
 		},
 	},
 
