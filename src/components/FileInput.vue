e9abc452331f473cd9e741932be4e4b1616f1a8c
rodrigo-barboza <rodrigoleandro.adm@gmail.com>
2024-06-27 14:06:38 -0300

    fix: corrige lógica de verificação dos tipos válidos

--- src/components/FileInput.vue
+++ src/components/FileInput.vue
@@ -270,9 +270,9 @@ export default {
 		isValid: {
 			handler(newValue) {
 				if (newValue) {
-					this.internalState = 'invalid';
-				} else {
 					this.internalState = 'valid';
+				} else {
+					this.internalState = 'invalid';
 				}
 			},
 			immediate: true,
