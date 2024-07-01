a0254e7d9d0143eac3e37439c68a74a496dfc6f3
johnn1sbo3s <joaopaulosncastro@gmail.com>
2024-07-01 16:43:41 -0300

    Implementa visualização da imagem e lógica de comportamento

--- src/components/FileViewer.vue
+++ src/components/FileViewer.vue
@@ -1,17 +1,20 @@
 <template>
 	<div
+		ref="fileViewer"
 		class="file-viewer"
 		:class="`file-viewer--${variant}`"
+		tabindex="0"
 		@mouseenter="handleMouseEnter"
 		@mouseleave="handleMouseLeave"
 		@click="handleClick"
+		@keyup.esc="handleClose"
 	>
 		<div class="file-viewer__image">
 			<div
 				v-if="!isHovered"
 				class="file-viewer__extension"
 			>
-				.jpg
+				{{ fileExtension }}
 			</div>
 			<div
 				v-else
@@ -30,13 +33,67 @@
 				{{ label }}
 			</div>
 			<div class="file-viewer__file-name">
-				{{ fileName }}
+				{{ fileUrl }}
 			</div>
 			<div
 				class="file-viewer__download"
 				:class="`file-viewer__download--${variant}`"
 			>
-				Baixar arquivo
+				<a @click.stop="emit('download-click', true)">
+					Baixar arquivo
+				</a>
+			</div>
+		</div>
+	</div>
+	<div
+		v-if="openVisualizer"
+		class="file-viewer__backdrop"
+	>
+		<div class="file-viewer__visualizer-header">
+			<div class="file-viewer__visualizer-label">
+				{{ label }}
+			</div>
+			<div class="file-viewer__visualizer-actions">
+				<div
+					class="file-viewer__visualizer-download"
+					@click="emit('download-click', true)"
+				>
+					<cds-icon
+						height="24"
+						width="24"
+						name="download-cloud-outline"
+					/>
+					Baixar arquivo
+				</div>
+				<cds-icon
+					class="file-viewer__visualizer-close"
+					height="24"
+					width="24"
+					name="x-outline"
+					@click="handleClose"
+				/>
+			</div>
+		</div>
+		<div
+			class="file-viewer__visualizer-image-container"
+			@click="handleClose"
+		>
+			<cds-image
+				v-if="allowedExtensions.includes(fileExtension)"
+				class="file-viewer__visualizer-image"
+				:src="fileUrl"
+				@click.stop=""
+			/>
+			<div
+				v-else
+				class="file-viewer__visualizer-empty-state"
+			>
+				<cds-image
+					src="https://i.imgur.com/d842Bek.png"
+					width="100"
+					height="100"
+				/>
+				Não foi possível gerar uma pré-visualização do arquivo.
 			</div>
 		</div>
 	</div>
@@ -44,25 +101,41 @@
 
 <script setup>
 import CdsIcon from '../components/Icon.vue';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import { colors } from '../utils/constants/colors';
+import CdsImage from '../components/Image.vue';
 
-const isHovered = ref(false);
+const emit = defineEmits(['close', 'download-click']);
 
-defineProps({
+const props = defineProps({
 	label: {
 		type: String,
 		default: 'Arquivo anexado',
 	},
-	fileName: {
+	fileUrl: {
 		type: String,
-		default: 'nome_do_arquivo.jpg',
+		required: true,
 	},
 	variant: {
 		type: String,
 		default: 'green',
 		validator: (value) => colors.find((color) => color.name === value),
 	},
+})
+
+const isHovered = ref(false);
+const openVisualizer = ref(false);
+const allowedExtensions = ['jpg', 'jpeg', 'png', 'webp'];
+
+const fileExtension = computed(() => {
+	if (props.fileUrl) {
+		let extension = (props.fileUrl.split('.')[props.fileUrl.split('.').length - 1]);
+		if (extension.length <= 4) {
+			return extension;
+		}
+	}
+
+	return 'file';
 })
 
 const handleMouseEnter = () => {
@@ -74,7 +147,18 @@ const handleMouseLeave = () => {
 }
 
 const handleClick = () => {
-	console.log('clicante');
+	openVisualizer.value = !openVisualizer.value;
+	mustDisableExternalScrolls(openVisualizer.value);
+}
+
+const handleClose = () => {
+	openVisualizer.value = false;
+	mustDisableExternalScrolls(openVisualizer.value);
+	emit('close', true);
+}
+
+const mustDisableExternalScrolls = (value) => {
+	document.body.style.overflow = value ? 'hidden' : 'auto';
 }
 
 </script>
@@ -94,6 +178,10 @@ const handleClick = () => {
 	gap: spacer(3);
 	transition: 0.3s ease;
 
+	&:focus {
+		outline: none;
+	}
+
 		@include variantResolver using ($color-name, $shade-50, $shade-100, $shade-200, $shade-300, $base-color, $shade-500, $shade-600) {
 
 				&:hover {
@@ -103,7 +191,6 @@ const handleClick = () => {
 
 				}
 		}
-
 
 	&__image {
 		width: 66px;
@@ -160,6 +247,93 @@ const handleClick = () => {
 		@include variantResolver using ($color-name, $shade-50, $shade-100, $shade-200, $shade-300, $base-color, $shade-500, $shade-600) {
 			color: $base-color;
 		}
+
+		&:hover {
+			text-decoration: underline;
+		}
+	}
+
+	&__backdrop {
+		width: 100%;
+		height: 100%;
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		z-index: $z-index-modal;
+		background: rgba(0,0,0, 0.85);
+		animation: ease .3s;
+	}
+
+	&__visualizer-header {
+		height: 64px;
+		width: 100%;
+		padding: pYX(5, 8);
+		background-color: rgba(0,0,0, 0.5);
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	&__visualizer-label {
+		@include subheading-2;
+		font-weight: $font-weight-bold;
+		color: $n-0;
+	}
+
+	&__visualizer-actions {
+		display: flex;
+		align-items: center;
+		gap: spacer(4);
+	}
+
+	&__visualizer-download {
+		display: flex;
+		align-items: center;
+		padding: pYX(2, 4);
+		border-radius: $border-radius-lil;
+		gap: spacer(2);
+		@include body-1;
+		font-weight: $font-weight-semibold;
+		color: $n-0;
+		cursor: pointer;
+
+		&:hover {
+			background-color: $n-900;
+		}
+	}
+
+	&__visualizer-image-container {
+		width: 100%;
+		height: 90vh;
+		padding: pa(5);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	&__visualizer-image {
+		max-height: 100%;
+		object-fit: cover;
+	}
+
+	&__visualizer-close {
+		cursor: pointer;
+		color: $n-0;
+	}
+
+	&__visualizer-empty-state {
+		display: flex;
+		flex-direction: column;
+		text-align: center;
+		justify-content: center;
+		align-items: center;
+		gap: spacer(3);
+		max-width: 250px;
+		color: $n-0;
+		@include subheading-3;
+		font-weight: $font-weight-regular;
 	}
 }
 
