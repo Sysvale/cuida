ea0cd8725d5f4cc45abe447730e4f82163c47629
johnn1sbo3s <joaopaulosncastro@gmail.com>
2024-08-23 15:34:58 -0300

    fix: Padroniza a propriedade de comparação de tamanho da tela

--- src/utils/methods/isDeviceType.js
+++ src/utils/methods/isDeviceType.js
@@ -2,19 +2,19 @@ export default (device) => {
 	switch (device) {
 		case 'smartphone':
 			// se a largura da tela for menor que 500px e menor que a altura da tela
-			return window.outerWidth < 500 && window.outerWidth < window.innerHeight;
+			return window.outerWidth < 500 && window.outerWidth < window.outerHeight;
 		case 'tablet':
 			// se a largura da tela estiver entre 500px e 800px, e for menor que a altura da tela
-			return window.outerWidth >= 500 && window.outerWidth < 800 && window.outerWidth < window.innerHeight;
+			return window.outerWidth >= 500 && window.outerWidth < 800 && window.outerWidth < window.outerHeight;
 		case 'hd-screen':
 			// se a largura da tela estiver entre 900px e 1500px, e maior que a altura da tela
-			return window.outerWidth > 900 && window.outerWidth < 1500 && window.outerWidth > window.innerHeight ;
+			return window.outerWidth > 900 && window.outerWidth < 1500 && window.outerWidth > window.outerHeight ;
 		case 'fhd-screen':
 			// se a largura da tela estiver entre 1600 e 2000, e maior que a altura da tela
-			return window.outerWidth > 1600 && window.outerWidth < 2000 && window.outerWidth > window.innerHeight;
+			return window.outerWidth > 1600 && window.outerWidth < 2000 && window.outerWidth > window.outerHeight;
 		case 'uhd-screen':
 			// se a largura da tela for maior que 2000, e maior que a altura da tela
-			return window.outerWidth > 2000 && window.outerWidth > window.innerHeight;
+			return window.outerWidth > 2000 && window.outerWidth > window.outerHeight;
 		default:
 			throw new Error(`Valor inválido: '${device}'. Os valores aceitos são: 'smartphone', 'tablet', 'hd-screen', 'fhd-screen', 'uhd-screen'.`);
 	}
