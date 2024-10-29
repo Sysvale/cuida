1ac8e8a05ffb65d4ebbacf4d74ca934edb4c4a37
johnn1sbo3s <joaopaulosncastro@gmail.com>
2024-10-29 15:36:29 -0300

    feat: Implementa lógica de focus, emits e close

--- src/components/GlobalSearchBar.vue
+++ src/components/GlobalSearchBar.vue
@@ -1,9 +1,13 @@
 <template>
 	<div
-		v-if="modelValue"
+		v-if="internalValue"
 		class="global-search-bar"
 	>
-		<div class="global-search-bar__backdrop">
+		<div
+			class="global-search-bar__backdrop"
+			@click="onBackdropClick"
+			@keyup.esc="internalValue = false"
+		>
 			<cds-flexbox
 				gap="3"
 				class="global-search-bar__input"
@@ -17,12 +21,15 @@
 				/>
 
 				<input
+					ref="searchInput"
+					v-model="searchTerm"
 					type="text"
 					placeholder="Buscar por nome, CPF ou CNS..."
 				>
 
 				<span
 					class="global-search-bar__clear"
+					@click="clearSearchTerm"
 				>
 					Limpar
 				</span>
@@ -30,28 +37,39 @@
 
 			<cds-flexbox
 				direction="column"
-				gap="1"
+				gap="4"
 				class="global-search-bar__results"
+				wrap="nowrap"
 			>
-				<span class="global-search-bar__divider">
-					Gestantes
-				</span>
-
-				<cds-flexbox
-					v-for="n in 5"
-					:key="n"
-					gap="1"
-					direction="column"
-					class="global-search-bar__result-card"
+				<div
+					v-for="item in items"
+					:key="item"
 				>
-					<span class="global-search-bar__person-name">
-						Adriana Pequena
-					</span>
+					<cds-divider
+						:text="item.category"
+						inline
+						class="global-search-bar__divider"
+					/>
 
-					<span class="global-search-bar__person-secondary-info">
-						<b>CPF:</b> 222.222.222-22
-					</span>
-				</cds-flexbox>
+					<div
+						v-for="result in item.results.slice(0, 5)"
+						:key="result"
+						class="global-search-bar__result-item"
+						@click="onItemClick(result)"
+					>
+						{{ result.title }}
+					</div>
+
+					<cds-flexbox
+						v-if="item.results.length > 5"
+						class="global-search-bar__show-more"
+						align="center"
+						justify="center"
+						@click="onSeeMoreClick(item)"
+					>
+						Ver todos os {{ item.results.length }} resultados
+					</cds-flexbox>
+				</div>
 			</cds-flexbox>
 		</div>
 	</div>
@@ -61,14 +79,82 @@
 import { ref, watch } from 'vue';
 import CdsIcon from '../components/Icon.vue';
 import CdsFlexbox from '../components/Flexbox.vue';
+import CdsDivider from '../components/Divider.vue';
 
-defineProps({
+const props = defineProps({
 	modelValue: {
 		type: Boolean,
 		default: false,
 		required: false,
+	},
+	items: {
+		type: Array,
+		required: false,
+		default: () => [],
 	}
 })
+
+const emits = defineEmits(['update:modelValue', 'change', 'onItemClick', 'onSeeMoreClick', 'close']);
+
+const internalValue = ref(props.modelValue);
+const idTimeout = ref(null);
+const searchInput = ref(null);
+const searchTerm = ref('');
+
+watch(searchTerm, () => {
+	onChangeSearchTerm();
+});
+
+watch(() => props.modelValue, (newValue) => {
+	internalValue.value = newValue;
+
+	if (newValue) {
+		setTimeout(() => {
+			searchInput.value?.focus();
+		}, 100);
+	}
+});
+
+watch(internalValue, (newValue) => {
+	if (!newValue) {
+		clearSearchTerm();
+		emits('close');
+	}
+
+	emits('update:modelValue', newValue);
+	mustDisableExternalScrolls(newValue);
+});
+
+function mustDisableExternalScrolls(value) {
+	document.body.style.overflow = value ? 'hidden' : 'auto';
+}
+
+function clearSearchTerm() {
+	searchTerm.value = '';
+	onChangeSearchTerm();
+}
+
+function onChangeSearchTerm() {
+	clearTimeout(idTimeout.value);
+
+	idTimeout.value = setTimeout(() => {
+		emits('change', searchTerm.value);
+	}, 1000);
+}
+
+function onItemClick(item) {
+	emits('onItemClick', item);
+}
+
+function onSeeMoreClick(item) {
+	emits('onSeeMoreClick', item);
+}
+
+function onBackdropClick(event) {
+	if (event.target === event.currentTarget) {
+		internalValue.value = false;
+	}
+}
 
 </script>
 
@@ -78,8 +164,11 @@ defineProps({
 .global-search-bar {
 
 	&__backdrop {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 		position: fixed;
-		padding-top: 170px;
+		padding-top: 100px;
 		top: 0;
 		bottom: 0;
 		left: 0;
@@ -101,6 +190,7 @@ defineProps({
 		border: 1px solid $n-30;
 		background-color: $n-0;
 		margin: mb(2);
+		box-shadow: $shadow-md;
 
 		input {
 			font-family: 'Satoshi', sans-serif;
@@ -121,6 +211,10 @@ defineProps({
 		font-weight: $font-weight-semibold;
 		color: $n-300;
 		cursor: pointer;
+
+		&:hover {
+			color: $n-500;
+		}
 	}
 
 	&__results {
@@ -129,35 +223,39 @@ defineProps({
 		border: 1px solid $n-30;
 		padding: py(5);
 		width: 70%;
+		max-height: 70svh;
 		box-shadow: $shadow-md;
+		overflow-y: scroll;
 	}
 
 	&__divider {
-		@include caption;
-		font-weight: $font-weight-semibold;
-		color: $n-600;
-		margin: ml(5);
+		padding: px(5);
 	}
 
-	&__result-card {
+	&__result-item {
 		cursor: pointer;
 		padding: pYX(3, 5);
+		@include body-1;
+		font-weight: $font-weight-regular;
+		color: $n-800;
 
 		&:hover {
 			background-color: $n-20;
 		}
 	}
 
-	&__person-name {
-		@include body-1;
-		font-weight: $font-weight-regular;
-		color: $n-800;
-	}
+	&__show-more {
+		@include caption;
+		color: $n-400;
+		text-align: center;
+		cursor: pointer;
+		height: 40px;
 
-	&__person-secondary-info {
-		@include body-2;
-		font-weight: $font-weight-regular;
-		color: $n-700;
+		&:hover {
+			background-color: $bn-50;
+			color: $bn-400;
+		}
 	}
 }
 </style>
+
