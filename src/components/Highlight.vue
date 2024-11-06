fbf72be6888630051fa3a07000ef4c12da04f8b3
Rafael Gondi <rdias.ga@gmail.com>
2024-11-06 11:18:12 -0300

    feat: Implementa feature de destaque em apenas uma porção do conteúdo enviado para o slot no componente Highlight

--- src/components/Highlight.vue
+++ src/components/Highlight.vue
@@ -1,50 +1,79 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
+	<template v-if="firstHalf">
+		{{ firstHalf }}
+	</template>
 	<span
 		class="highlight__container"
 		:class="dynamicHighlightClass"
 		:style="dynamicStyle"
 	>
-		<!-- @slot Slot usado para especificar o texto que receberá o highlight. -->
-		<slot />
+		<template v-if="highlightedText">
+			{{ highlightedText }}
+		</template>
+		<template v-else>
+			<!-- @slot Slot usado para especificar o texto que receberá o highlight. -->
+			<slot />
+		</template>
 	</span>
+	<template v-if="secondHalf">
+		{{ secondHalf }}
+	</template>
 </template>
 
 <script>
 export default {
 	props: {
 		/**
-		 * A variante do Highlight. São 3 variantes implementadas: 'info', 'success'
-		 * e 'danger'
-		 */
+		* A variante do Highlight. São 3 variantes implementadas: 'info', 'success'
+		* e 'danger'
+		*/
 		variant: {
 			type: String,
 			default: 'info',
 		},
 		/**
-		 * Especifica se o highlight vai ser estático ou animado.
-		 */
+		* Especifica se o highlight vai ser estático ou animado.
+		*/
 		animated: {
 			type: Boolean,
 			default: false,
 		},
 		/**
-		 * Especifica a duração da animação.
-		 */
+		* Especifica a duração da animação.
+		*/
 		duration: {
 			type: Number,
 			default: 1,
 		},
 		/**
-		 * O tempo de espera até a animação começar.
-		 */
+		* O tempo de espera até a animação começar.
+		*/
 		delay: {
 			type: Number,
 			default: 0,
 		},
+		/**
+		* Define uma substring específica do texto enviada por slot que deve receber o destaque (highlight).
+		*/
+		highlightedText: {
+			type: [String, null],
+			default: null,
+		},
+	},
+
+	data() {
+		return {
+			firstHalf: '',
+			secondHalf: '',
+		}
 	},
 
 	computed: {
+		hasSlots() {
+			return !!Object.keys(this.$slots).length;
+		},
+
 		dynamicHighlightClass() {
 			let dynamicClass = '';
 
@@ -69,6 +98,24 @@ export default {
 				'--duration': `${this.duration}s`,
 				'--delay': `${this.delay}s`,
 			};
+		},
+	},
+
+	watch: {
+		highlightedText() {
+			this.splitContent();
+		}
+	},
+
+	mounted() {
+		this.splitContent();
+	},
+
+	methods: {
+		splitContent() {
+			if (this.hasSlots && this.highlightedText ) {
+				[this.firstHalf, this.secondHalf] = this.$slots.default()[0].children.split(this.highlightedText);
+			}
 		},
 	},
 };
