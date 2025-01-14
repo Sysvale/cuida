acb9843434c08a48718fba012555a673ea07da51
Rafael Gondi <rdias.ga@gmail.com>
2025-01-14 12:56:41 -0300

    refactor: Remove código comentado e passa o SearchInput para Composition API

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,179 +1,88 @@
 <template>
-	<div>
-		<div :class="stepperInputDynamicClass">
-			<cds-icon
-				name="search-outline"
-				class="search-input__search-icon"
-			/>
-
-			<input
-				id="cds-search-input"
-				:value="internalValue"
-				:placeholder="placeholder"
-				:disabled="disabled"
-				:class="inputClass"
-				@focus="isBeingFocused = true"
-				@blur="isBeingFocused = false"
-				@input="handleInput"
-			>
-
-			<!-- <cds-link-button
-				v-if="internalValue"
-				variant="blue"
-				@click="internalValue = ''"
-			>
-				Limpar
-			</cds-link-button> -->
-
-			<cds-icon
-				v-if="internalValue"
-				name="x-outline"
-				width="18"
-				height="18"
-				class="search-input__close-icon"
-				@click="handleClearInput"
-			/>
-
-			<!-- <div class="search-input__icon-container">
-				<check-icon
-					v-if="validState && !disabled"
-					size="1x"
-					class="search-input__icon--check-icon"
-				/>
-				<alert-circle-icon
-					v-if="errorState && !disabled"
-					size="1x"
-					class="search-input__icon--alert-circle-icon"
-				/>
-				<cds-spinner
-					v-if="loadingState && !disabled"
-					size="sm"
-					variant="blue"
-					class="search-input__icon--spinner-icon"
-				/>
-			</div> -->
-		</div>
-		<!-- <div
-			v-if="errorState && !disabled"
-			class="search-input__error-message"
+	<div :class="stepperInputDynamicClass">
+		<cds-icon
+			name="search-outline"
+			class="search-input__search-icon"
+		/>
+		<input
+			id="cds-search-input"
+			:value="internalValue"
+			:placeholder="placeholder"
+			:disabled="disabled"
+			:class="inputClass"
+			@focus="isBeingFocused = true"
+			@blur="isBeingFocused = false"
+			@input="handleInput"
 		>
-			{{ errorMessage }}
-		</div> -->
+
+		<cds-icon
+			v-if="internalValue"
+			name="x-outline"
+			width="18"
+			height="18"
+			class="search-input__close-icon"
+			@click="handleClearInput"
+		/>
 	</div>
 </template>
 
-<script>
+<script setup>
+import { ref, computed, onMounted } from 'vue';
 import CdsIcon from './Icon.vue';
 import CdsLinkButton from './FlatButton.vue';
 
-export default {
-	components: {
-		CdsIcon,
-		CdsLinkButton,
+const internalValue = ref('');
+const isBeingFocused = ref(false);
+
+const model = defineModel();
+
+const props = defineProps({
+	/**
+	* Desabilita o SearchInput.
+	*/
+	disabled: {
+		type: Boolean,
+		default: false,
 	},
-
-	props: {
-		/**
-		* Prop utilizada como v-model.
-		*/
-		modelValue: {
-			type: [String, Number],
-			default: '',
-		},
-		/**
-		 * Desabilita o input.
-		 */
-		disabled: {
-			type: Boolean,
-			default: false,
-		},
-		/**
-		 * Especifica o placeholder do input
-		 */
-		placeholder: {
-			type: String,
-			default: 'Busque...',
-		},
-		/**
-		 * Especifica se a largura do TextInput deve ser fluida.
-		 */
-		fluid: {
-			type: Boolean,
-			default: false,
-			required: false,
-		},
+	/**
+	* Especifica o placeholder do input
+	*/
+	placeholder: {
+		type: String,
+		default: 'Busque...',
 	},
-
-	data() {
-		return {
-			internalValue: '',
-			isBeingFocused: false,
-		};
+	/**
+	* Especifica se a largura do SearchInput deve ser fluida.
+	*/
+	fluid: {
+		type: Boolean,
+		default: false,
 	},
+});
 
-	computed: {
-		hasSlots() {
-			return !!Object.keys(this.$slots).length;
-		},
+const stepperInputDynamicClass = computed(() => {
+	let stepperInputClass = props.fluid ? 'search-input--fluid' : 'search-input';
+	return stepperInputClass.concat(' ', props.disabled ? 'search-input--disabled' : '');
+});
 
-		stepperInputDynamicClass() {
-			let stepperInputClass = this.fluid ? 'search-input--fluid' : 'search-input';
-			return stepperInputClass.concat(' ', this.disabled ? 'search-input--disabled' : '');
+const inputClass = computed(() => props.fluid ? 'search-input__field--fluid' : 'search-input__field');
 
-			// if (!this.isBeingFocused) {
-			// 	if (!this.disabled) {
-			// 		if (this.state === 'valid') {
-			// 			stepperInputClass += ' search-input--valid';
-			// 		} else if (this.state === 'invalid') {
-			// 			stepperInputClass += ' search-input--invalid';
-			// 		}
-			// 	} else {
-			// 		stepperInputClass += ' search-input--disabled';
-			// 	}
-			// } else if (!this.disabled) {
-			// 	if (this.state === 'default') {
-			// 		stepperInputClass += ' search-input--focused';
-			// 	} else if (this.state === 'valid') {
-			// 		stepperInputClass += ' search-input--focused-valid';
-			// 	} else if (this.state === 'invalid') {
-			// 		stepperInputClass += ' search-input--focused-invalid';
-			// 	} else if (this.state === 'loading') {
-			// 		stepperInputClass += ' search-input--focused-loading';
-			// 	}
-			// }
-		},
 
-		// loadingState(){
-		// 	return this.state === 'loading';
-		// },
-
-		inputClass() {
-			return this.fluid ? 'search-input__field--fluid' : 'search-input__field';
-		},
-	},
-
-	mounted() {
-		this.internalValue = this.modelValue;
-	},
-
-	methods: {
-		handleInput(e) {
-			this.internalValue = e.target.value;
-			/**
-			* Evento utilizado para implementar o v-model.
-			* @event update:modelValue
-			* @type {Event}
-			*/
-			this.$emit('update:modelValue', e.target.value);
-		},
-
-		handleClearInput() {
-			this.internalValue = '';
-			this.$emit('update:modelValue', '');
-		},
-	},
+function handleInput(e) {
+	internalValue.value = e.target.value;
+	model.value = e.target.value;
 };
+
+function handleClearInput() {
+	internalValue.value = '';
+	model.value = '';
+};
+
+onMounted(() => {
+	internalValue.value = model.value;
+});
 </script>
+
 <style lang="scss" scoped>
 @import '../assets/sass/tokens.scss';
 
@@ -184,7 +93,6 @@ export default {
 	border-radius: $border-radius-extra-small;
 	width: fit-content;
 	width: -moz-fit-content;
-	margin: mt(2);
 	background: $n-0;
 
 	&--fluid {
@@ -207,28 +115,6 @@ export default {
 		color: $n-400;
 	}
 
-	&__label {
-		@include body-2;
-		font-weight: $font-weight-semibold;
-		color: $n-700;
-		display: flex;
-		align-items: flex-end;
-	}
-
-	&__icon {
-		margin: ml(1);
-		cursor: pointer;
-	}
-
-	&__icon-container {
-		background-color: none;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		margin: mr(3);
-		min-width: 15px;
-	}
-
 	&__field {
 		padding: pTRBL(3, 3, 3, 2);
 		margin: mr(2);
@@ -249,43 +135,10 @@ export default {
 		}
 	}
 
-	&--focused {
-		@extend .search-input;
-		outline: 1px solid $bn-300;
-		box-shadow: 0 0 0 0.2rem rgba($bn-300, .45);
-	}
-
 	&--disabled {
 		background-color: $n-20;
 		pointer-events: none;
 		border: none;
 	}
-
-	&__icon--spinner-icon {
-		padding: 0px;
-	}
-}
-
-input::-webkit-outer-spin-button,
-input::-webkit-inner-spin-button {
-	-webkit-appearance: none;
-	margin: ma(0);
-}
-
-input:disabled {
-	background: none !important;
-}
-
-input::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
-	color: $n-400;
-	opacity: 1; /* Firefox */
-}
-
-input:-ms-input-placeholder { /* Internet Explorer 10-11 */
-	color: $n-400;
-}
-
-input::-ms-input-placeholder { /* Microsoft Edge */
-	color: $n-400;
 }
 </style>
