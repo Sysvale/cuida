23eff33b60999fc32125c4fde18611aa6693dd3d
“RafaelGondi” <rdias.ga@gmail.com>
2025-02-14 16:35:07 -0300

    feat: adiciona eventos change e keydown no useComponentEmits

--- src/utils/composables/useComponentEmits.js
+++ src/utils/composables/useComponentEmits.js
@@ -1,26 +1,29 @@
 export function nativeEmits(emits = null) {
 	const emitBlur = () => emits('blur');
-	const emitFocus = () => emits('focus');
 	const emitChange = () => emits('change');
 	const emitClick = () => emits('click');
+	const emitFocus = () => emits('focus');
+	const emitKeydown = (event) => emits('keydown', event);
 	const emitMouseenter = () => emits('mouseenter');
 	const emitMouseleave = () => emits('mouseleave');
 
 	return {
 		emitBlur,
-		emitFocus,
 		emitChange,
 		emitClick,
+		emitFocus,
+		emitKeydown,
 		emitMouseenter,
-		emitMouseleave
+		emitMouseleave,
 	};
 }
 
 export const nativeEvents = {
 	'blur': null,
-	'focus': null,
 	'change': null,
 	'click': null,
+	'focus': null,
+	'keydown': null,
 	'mouseenter': null,
 	'mouseleave': null,
 };
