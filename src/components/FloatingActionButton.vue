77bdc9d7fe1adb8a4a443b0af81ce9ebf247155d
johnn1sbo3s <joaopaulosncastro@gmail.com>
2025-02-17 15:52:49 -0300

    feat: Adiciona subitems

--- src/components/FloatingActionButton.vue
+++ src/components/FloatingActionButton.vue
@@ -1,13 +1,27 @@
 <template>
-	<div
-		class="floating-action-button"
-		:class="`floating-action-button--${variant}`"
-	>
-		<icon
-			:name="icon"
-			height="20"
-			width="20"
-		/>
+	<div class="floating-action-button__container">
+		<div
+			v-for="subitem in 3"
+			:key="subitem"
+			class="floating-action-button__subitem"
+		>
+			<icon
+				name="home-outline"
+				height="32"
+				width="32"
+			/>
+		</div>
+
+		<div
+			class="floating-action-button__main-button"
+			:class="`floating-action-button__main-button--${variant}`"
+		>
+			<icon
+				:name="icon"
+				height="20"
+				width="20"
+			/>
+		</div>
 	</div>
 </template>
 
@@ -38,35 +52,86 @@ const props = defineProps({
 @import '../assets/sass/tokens.scss';
 
 .floating-action-button {
-	position: absolute;
-	bottom: 0;
-	right: 0;
-	margin: 0 16px 20px 0;
-	border-radius: $border-radius-medium;
-	width: 50px;
-	height: 50px;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	padding: pa(3);
-	color: white;
-	box-shadow: $shadow-md;
-	cursor: pointer;
-	transition: background-color 0.2s ease;
 
-	@include variantResolver using ($color-name, $shade-50, $shade-100, $shade-200, $shade-300, $base-color, $shade-500, $shade-600) {
-		background-color: $base-color;
+	&__container {
+		position: absolute;
+		bottom: 0;
+		right: 0;
+		margin: 0 16px 20px 0;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: spacer(3);
 	}
 
-	&:active::before {
-		content: "";
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-		background-color: rgba(0, 0, 0, 0.2);
+	&__main-button {
+		position: relative;
 		border-radius: $border-radius-medium;
+		width: 50px;
+		height: 50px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		padding: pa(3);
+		color: $n-0;
+		box-shadow: $shadow-md;
+		cursor: pointer;
+		transition: background-color 0.2s ease;
+
+		@include variantResolver using ($color-name, $shade-50, $shade-100, $shade-200, $shade-300, $base-color, $shade-500, $shade-600) {
+			background-color: $base-color;
+		}
+
+		&::before {
+			content: "";
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			background-color: rgba(0, 0, 0, 0.1);
+			border-radius: $border-radius-medium;
+			opacity: 0;
+			transition: opacity 0.2s ease;
+		}
+
+		&:active::before {
+			opacity: 1;
+		}
+	}
+
+	&__subitem {
+		position: relative;
+		border-radius: $border-radius-small;
+		width: 30px;
+		height: 30px;
+		margin: mt(n1);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		padding: pa(2);
+		background-color: $n-0;
+		color: $n-800;
+		box-shadow: $shadow-sm;
+		cursor: pointer;
+		transition: background-color 0.2s ease;
+
+		&::before {
+			content: "";
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			background-color: rgba(0, 0, 0, 0.1);
+			border-radius: $border-radius-small;
+			opacity: 0;
+			transition: opacity 0.2s ease;
+		}
+
+		&:active::before {
+			opacity: 1;
+		}
 	}
 }
 
