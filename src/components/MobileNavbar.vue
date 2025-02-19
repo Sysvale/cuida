3930a0259edcaf020994a3070359d46c98a4997f
johnn1sbo3s <joaopaulosncastro@gmail.com>
2025-02-19 14:07:07 -0300

    docs: ajusta documentação para mostrar os emits e as props iniciais

--- src/components/MobileNavbar.vue
+++ src/components/MobileNavbar.vue
@@ -22,7 +22,7 @@
 					/>
 
 					<div
-						v-if="!noLabel"
+						v-if="showLabel"
 						class="mobile-navbar__item-text"
 					>
 						{{ item.label }}
@@ -59,14 +59,14 @@ const props = defineProps({
 	 */
 	variant: {
 		type: String,
-		default: 'blue',
+		default: 'green',
 	},
 	/**
 	 * Remove as labels dos itens da navbar.
 	 */
-	noLabel: {
+	showLabel: {
 		type: Boolean,
-		default: true,
+		default: false,
 	},
 });
 
@@ -150,7 +150,7 @@ function onItemClick(item, index) {
 		flex: 1;
 		min-width: 0;
 		height: 100%;
-		min-height: 45px;
+		min-height: 50px;
 		padding: pYX(2, 1);
 		position: relative;
 		color: $n-600;
