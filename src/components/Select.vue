4b16e9af51c90845fa382036bdcb0985a64188f8
“RafaelGondi” <rdias.ga@gmail.com>
2025-02-25 14:34:50 -0300

    fix: ajusta erro de larguram com a prop fluid

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -304,6 +304,10 @@ const selectOptionsClass = computed(() => ({
 	'select__options--up': direction.value === 'up',
 }));
 
+const selectContainerWidth = computed(() => {
+	return props.fluid ? '100%' : 'fit-content';
+})
+
 
 //NOTE: Essa computada vai ser removida junto com a descontinuação da prop width na V4
 const computedFluid = computed(() => {
@@ -514,7 +518,7 @@ defineExpose({
 @import '../assets/sass/tokens.scss';
 
 .select {
-	width: fit-content;
+	width: v-bind(selectContainerWidth);
 
 	&__input {
 		&--searchable {
