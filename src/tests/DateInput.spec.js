356e53a075cf1eb9d14c78e81d68e727da304ea4
“RafaelGondi” <rdias.ga@gmail.com>
2025-02-27 15:16:53 -0300

    test: atualiza teste e cria teste de snapshot

--- src/tests/DateInput.spec.js
+++ src/tests/DateInput.spec.js
@@ -1,9 +1,10 @@
-import { describe, test, expect, beforeEach } from 'vitest';
+import { describe, test, expect, beforeEach, vi } from 'vitest';
 import { mount } from '@vue/test-utils';
-import DateInput from '@/components/DateInput.vue'; // Ajuste o caminho conforme necessário
+import DateInput from '../components/DateInput.vue'; // Ajuste o caminho conforme necessário
+import CdsBaseInput from '../components/BaseInput.vue';
 import { DateTime } from 'luxon';
 
-describe('DateInput.vue', () => {
+describe('DateInput', () => {
 	let wrapper;
 
 	beforeEach(() => {
@@ -15,12 +16,84 @@ describe('DateInput.vue', () => {
 		});
 	});
 
-	test('renderiza corretamente', () => {
-		expect(wrapper.find('.date-input').exists()).toBe(true);
-		expect(wrapper.find('input').attributes('placeholder')).toBe('dd/MM/yyyy');
+	test('renders correctly', () => {
+		expect(wrapper.html()).toMatchSnapshot();
 	});
 
-	test('abre e fecha o calendário ao clicar no input', async () => {
+	test('renders correctly with default props', () => {
+		expect(wrapper.findComponent(CdsBaseInput).exists()).toBe(true);
+		expect(wrapper.findComponent(CdsBaseInput).props('label')).toBe('Selecione uma data');
+		expect(wrapper.findComponent(CdsBaseInput).props('placeholder')).toBe('dd/MM/yyyy');
+	});
+
+	test('renders with a required indicator', () => {
+		wrapper = mount(DateInput, {
+			props: {
+				label: 'Selecione uma data',
+				required: true,
+			},
+		});
+
+		expect(wrapper.findComponent(CdsBaseInput).props('required')).toBe(true);
+	});
+
+	test('renders with disabled attribute when disabled prop is true', () => {
+		wrapper = mount(DateInput, {
+			props: {
+				label: 'Selecione uma data',
+				disabled: true,
+			},
+		});
+
+		expect(wrapper.findComponent(CdsBaseInput).props('disabled')).toBe(true);
+	});
+
+	test('does not render with disabled attribute when disabled prop is false', () => {
+		wrapper = mount(DateInput, {
+			props: {
+				label: 'Selecione uma data',
+				disabled: false,
+			},
+		});
+
+		expect(wrapper.findComponent(CdsBaseInput).props('disabled')).toBe(false);
+	});
+
+	test('renders with a custom label', () => {
+		wrapper = mount(DateInput, {
+			props: {
+				label: 'Data Personalizada',
+			},
+		});
+
+		expect(wrapper.findComponent(CdsBaseInput).props('label')).toBe('Data Personalizada');
+	});
+
+	test('renders with a support link', () => {
+		wrapper = mount(DateInput, {
+			props: {
+				label: 'Selecione uma data',
+				supportLink: 'Support Link',
+				supportLinkUrl: 'https://example.com',
+			},
+		});
+
+		expect(wrapper.findComponent(CdsBaseInput).props('supportLink')).toBe('Support Link');
+		expect(wrapper.findComponent(CdsBaseInput).props('supportLinkUrl')).toBe('https://example.com');
+	});
+
+	test('renders with a floating label when floatingLabel prop is true', () => {
+		wrapper = mount(DateInput, {
+			props: {
+				label: 'Selecione uma data',
+				floatingLabel: true,
+			},
+		});
+
+		expect(wrapper.findComponent(CdsBaseInput).props('floatingLabel')).toBe(true);
+	});
+
+	test('opens and closes the calendar when clicking the input', async () => {
 		await wrapper.find('input').trigger('click');
 		expect(wrapper.find('.date-input__calendar').exists()).toBe(true);
 
@@ -28,7 +101,7 @@ describe('DateInput.vue', () => {
 		expect(wrapper.find('.date-input__calendar').exists()).toBe(false);
 	});
 
-	test('navega entre os meses', async () => {
+	test('navigates between months', async () => {
 		await wrapper.find('input').trigger('click');
 		const currentMonth = wrapper.find('.calendar__month-and-title').text();
 
@@ -40,15 +113,7 @@ describe('DateInput.vue', () => {
 		expect(wrapper.find('.calendar__month-and-title').text()).toBe(currentMonth);
 	});
 
-	test('seleciona uma data', async () => {
-		await wrapper.find('input').trigger('click');
-		const today = DateTime.now().day;
-
-		await wrapper.find(`.calendar__day:not(.disabled-day)`).trigger('click');
-		expect(wrapper.find('input').element.value).toBe(DateTime.now().toFormat('dd/MM/yyyy'));
-	});
-
-	test('desabilita datas fora do intervalo permitido', async () => {
+	test('disables dates outside the allowed range', async () => {
 		await wrapper.setProps({ minDate: '2023-10-01', maxDate: '2023-10-31' });
 		await wrapper.find('input').trigger('click');
 
@@ -58,20 +123,5 @@ describe('DateInput.vue', () => {
 		const firstDisabledDay = disabledDays[0];
 		await firstDisabledDay.trigger('click');
 		expect(wrapper.find('input').element.value).not.toBe(firstDisabledDay.text());
-	});
-
-	test('seleciona um intervalo de datas', async () => {
-		await wrapper.setProps({ range: true });
-		await wrapper.find('input').trigger('click');
-
-		const startDay = wrapper.find('.calendar__day:not(.disabled-day)');
-		await startDay.trigger('click');
-
-		const endDay = wrapper.findAll('.calendar__day:not(.disabled-day)')[5];
-		await endDay.trigger('click');
-
-		const startDate = DateTime.now().toFormat('dd/MM/yyyy');
-		const endDate = DateTime.now().plus({ days: 5 }).toFormat('dd/MM/yyyy');
-		expect(wrapper.find('input').element.value).toBe(`De ${startDate} a ${endDate}`);
 	});
 });
