3d3609e3cc71c502a74bb378a5892c7b191d14d6
IhGori <igor.vinicius@discente.univasf.edu.br>
2025-03-12 08:42:58 -0300

    refactor: Ajusta altura para tamanho fixo

--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -191,7 +191,7 @@ function handleClick() {
 			}
 		}
 
-		&--md {
+		&--md {djsioajdisoa
 			.cds-tile__icon {
 				width: 50px;
 				height: 50px;
@@ -214,6 +214,10 @@ function handleClick() {
 
 	&__text {
 		text-align: center;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 50px;
 		@include caption;
 		font-weight: $font-weight-semibold;
 
