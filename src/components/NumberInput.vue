3235c5be144c1c2966ac0b90b079ebec2e0e946a
rdias.ga <rdias.ga@gmail.com>
2025-04-15 18:15:00 +0000

    fix: remove disableAutocomplete

--- src/components/NumberInput.vue
+++ src/components/NumberInput.vue
@@ -25,7 +25,6 @@
 			:floating-label="floatingLabel || mobile"
 			:support-link="supportLink || linkText"
 			:support-link-url="supportLinkUrl || linkUrl"
-			disableAutocomplete
 			@click="emitClick"
 			@change="handleChange"
 			@focus="handleFocus"
