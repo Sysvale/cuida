9a19201bc1101d871eb4e6a6e6d0b3bc259ce9ea
johnn1sbo3s <joaopaulosncastro@gmail.com>
2025-07-24 12:45:57 -0300

    fix: escreve a função hasSameItems em typescript

--- src/utils/methods/hasSameItems.ts
+++ src/utils/methods/hasSameItems.ts
@@ -1,5 +1,6 @@
-export default (arr1, arr2) => {
+export default function hasSameItems(arr1: string[], arr2: string[]): boolean {
 	if (arr1.length !== arr2.length) return false;
+	if (arr1.length === 0 && arr2.length === 0) return true;
 
 	const sorted1 = [...arr1].sort();
 	const sorted2 = [...arr2].sort();
