f3aa78de6e6db15eace6739b4045a93f72cf834d
“RafaelGondi” <rdias.ga@gmail.com>
2025-08-31 21:42:46 -0300

    Atualiza componente PageLayout

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -213,7 +213,7 @@ export default defineConfig({
 						{ text: 'MobileNavigation', link: '/components/navegação/mobile-navigation' },
 						// { text: 'MobileStepperInput', link: '/components/navegação/mobile-stepper-input' },
 						{ text: 'NavBar <Badge class="badge" type="info">ready</Badge>', link: '/components/navegação/nav-bar' },
-						{ text: 'PageLayout', link: '/components/navegação/page-layout' },
+						{ text: 'PageLayout <Badge class="badge" type="info">ready</Badge>', link: '/components/navegação/page-layout' },
 						{ text: 'Pagination <Badge class="badge" type="info">ready</Badge>', link: '/components/navegação/pagination' },
 						// { text: 'SecondaryNavigation', link: '/components/navegação/secondary-navigation' },
 						{ text: 'SegmentedControl <Badge class="badge" type="info">ready</Badge>', link: '/components/navegação/segmented-control' },
