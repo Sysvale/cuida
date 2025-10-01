6474191ea69f0105e02c1f04979a4928fc5dacf5
RafaelGondi <rdias.ga@gmail.com>
2025-10-01 10:24:56 -0300

    Adiciona link para a documentação em Storybook

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -27,6 +27,7 @@ export default defineConfig({
 			{ text: 'Utils', link: '/utils/documentation-guide' },
 			{ text: 'Ícones', link: '/foundation/iconografia' },
 			// { text: 'Brand', link: '/utils/' },
+			{ text: 'Docs Storybook/Framer', link: 'https://cuida.framer.wiki/' },
 			{ text: 'Cuida 2', link: 'https://sysvale.github.io/cuidaV2/?path=/docs/funda%C3%A7%C3%A3o-princ%C3%ADpios--page' }
 		],
 
