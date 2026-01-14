6a185572060c8063b1a4d96e6ba3cf5d1a647fee
jvictordev1 <jvictordev1@gmail.com>
2026-01-14 16:39:01 -0300

    feat: atualiza componente para composition api e adiciona prop de variant

--- src/components/Breadcrumb.vue
+++ src/components/Breadcrumb.vue
@@ -12,7 +12,7 @@
 				>
 					<router-link
 						:to="routerPushTo(item)"
-						class="breadcrumb__link"
+						:class="`breadcrumb__link--${variant}`"
 					>
 						<CdsIcon
 							v-if="items.length <= 2"
@@ -49,7 +49,7 @@
 
 					<router-link
 						:to="routerPushTo(item)"
-						class="breadcrumb__link"
+						:class="`breadcrumb__link--${variant}`"
 					>
 						<CdsClickable :clickable="index !== items.length - 1">
 							{{ item.label }}
@@ -61,43 +61,43 @@
 	</nav>
 </template>
 
-<script>
+<script setup>
 import CdsIcon from './Icon.vue';
 import CdsSpacer from './Spacer.vue';
 import CdsClickable from './Clickable.vue';
 import isEmpty from 'lodash.isempty';
 
-export default {
-	name: 'CdsBreadcrumb',
-	components: {
-		CdsIcon,
-		CdsSpacer,
-		CdsClickable,
-	},
-	props: {
-		/**
-		 * Define a lista dos itens do Breadcrumb a serem
-		 * mostrados. Os itens da lista devem ser
-		 * objetos com path ou route, e com uma label.
-		 */
-		items: {
-			type: Array,
-			default: () => ([]),
-			required: true,
-		},
-	},
+defineOptions({ name: 'CdsBreadcrumb' });
 
-	methods: {
-		resolveRoute({ route, path }) {
-			const to = isEmpty(route) ? path : route;
-			return to instanceof String ? { path: to } : to;
-		},
-
-		routerPushTo(item) {
-			return this.resolveRoute(item) ? this.resolveRoute(item).path : null;
-		},
+defineProps({
+	/**
+	 * Define a lista dos itens do Breadcrumb a serem
+	 * mostrados. Os itens da lista devem ser
+	 * objetos com path ou route, e com uma label.
+	 */
+	items: {
+		type: Array,
+		default: () => ([]),
+		required: true,
 	},
-};
+	/**
+	 * A variante de cor. São 10 variantes:
+	* @values green, teal, blue, indigo, violet, pink, red, orange, amber, dark
+	 */
+	variant: {
+		type: String,
+		default: 'blue',
+	},
+});
+
+function resolveRoute({ route, path }) {
+	const to = isEmpty(route) ? path : route;
+	return to instanceof String ? { path: to } : to;
+}
+
+function routerPushTo(item) {
+	return resolveRoute(item) ? resolveRoute(item).path : null;
+}
 </script>
 
 <style lang="scss" scoped>
@@ -113,18 +113,30 @@ export default {
 	}
 
 	&__link {
-		@extend .breadcrumb;
-		@include tokens.caption;
-		color: tokens.$n-600;
-		border-radius: tokens.$border-radius-lil;
-		padding: tokens.pa(1);
-		transition: tokens.$hover;
+		@include tokens.variantResolver using (
+			$color-name,
+			$shade-50,
+			$shade-100,
+			$shade-200,
+			$shade-300,
+			$base-color,
+			$shade-500,
+			$shade-600
+		) {
+			@extend .breadcrumb;
+			@include tokens.caption;
 
-		&:hover,
-		&:hover svg {
-			background-color: tokens.$bn-50;
-			color: tokens.$bn-600;
+			border-radius: tokens.$border-radius-lil;
+			padding: tokens.pa(1);
 			transition: tokens.$hover;
+			color: tokens.$n-600;
+
+			&:hover,
+			&:hover svg {
+				background-color: $shade-50;
+				color: $shade-600;
+				transition: tokens.$hover;
+			}
 		}
 	}
 
